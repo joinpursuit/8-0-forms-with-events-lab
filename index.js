9c1581671eb80e16fbc3b887613e61453329d404
JoseBorbon <joserborbon@gmail.com>
2021-09-24 10:41:41 -0400

    added events and elements to indexJS

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log("Code your solution!")
+document.querySelector("h1").textContent = "My To-Dos";
+const body = document.querySelector("body");
+const form = document.createElement("form");
+const textInput = document.createElement("input");
+const pEl = document.createElement("p");
+const button = document.createElement("button");
+textInput.setAttribute("type", "text");
+textInput.setAttribute("id", "textInput");
+
+button.setAttribute("type", "submit");
+button.textContent = "Submit";
+button.style["margin-left"] = "10px";
+
+body.append(document.createElement("hr"));
+body.append(pEl);
+
+pEl.style.display = "none";
+pEl.textContent = "Text Field Can't be Empty";
+
+body.append(form);
+form.append(textInput);
+form.append(button);
+
+body.append(document.createElement("ul"));
+
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  //   conditional used to toggle hidden error paragraph element
+  if (textInput.value.length === 0 && pEl.style.display === "none") {
+    pEl.style.display = "block";
+  } else if (textInput.value.length > 0) {
+    pEl.style.display = "none";
+    let li = document.createElement("li");
+    li.textContent = textInput.value;
+    document.querySelector("ul").appendChild(li);
+  }
+  form.reset();
+});
+
+document.querySelector("ul").addEventListener("click", (event) => {
+  event.preventDefault();
+  event.target.style["text-decoration"] = "line-through";
+});
