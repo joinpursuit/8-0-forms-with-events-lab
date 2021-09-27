cf3f077227aaaa9fe63268135165552d368eb782
Jalamang <Jalamangjanka@gmail.com>
2021-09-27 19:36:46 -0400

    Added and append elementts

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //console.log("Code your solution!")
+const body = document.querySelector('body');
+const form = document.createElement('form');
+form.setAttribute('id', 'form-content')
+const h1 = document.createElement('h1');
+h1.textContent = "My To-Dos";
+body.append(h1);
+
+
+const input = document.createElement('input');
+input.setAttribute('name', 'userInput')
+input.setAttribute('id', 'name')
+input.setAttribute('type', 'text')
+form.append(input);
