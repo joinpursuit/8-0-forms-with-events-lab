a52324b790cd612682fa17de19aeb5f61c51bcf6
Lakisha J <lakishajohnson@pursuit.org>
2021-09-27 19:33:39 -0400

    Added elements and attributes

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const body = document.querySelector('body');
+const form = document.createElement('form');
+form.setAttribute('id', 'form-content')
+const h1 = document.createElement('h1');
+h1.textContent = "My To-Dos";
+body.append(h1);
 
+
+const input = document.createElement('input');
+input.setAttribute('name', 'userInput')
+input.setAttribute('id', 'name')
+input.setAttribute('type', 'text')
+form.append(input);
 
 
 
