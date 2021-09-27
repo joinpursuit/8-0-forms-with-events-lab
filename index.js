d6bdaf35cb6e028b546da37ce7842610b62245ef
Mukayila ALAO <mukayilaalao@pursuit.org>
2021-09-27 13:59:19 -0400

    First part done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("Code your solution!")
+let myForm=document.querySelector("#form-id");
+myForm.addEventListener("submit", e=> {
+    e.preventDefault();
+    let ul=document.querySelector("ul");
+    let li=document.createElement("li");
+    if(e.target.todoslists.value) {
+        li.textContent=e.target.todoslists.value;
+        e.target.todoslists.value="";
+        ul.append(li);
+        li.addEventListener("click", e=>{
+            e.target.style.textDecoration="line-through";
+        })
+    }else {
+        let p=document.createElement("p");
+        p.textContent="Error:text should not be empty! ";
+        myForm.after(p);
+    }
+})
