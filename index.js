06bb05c4493dadff9dd7bf59fecb6d080c3aff33
rae charles <r_charles87@yahoo.com>
2021-09-27 15:30:01 -0400

    still working on bonus stuff

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Code your solution!")
+let userInput = document.querySelector("#user-form");
+
+
+userInput.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    let ul = document.querySelector("ul");
+    let li = document.createElement("li");
+    let todo = event.target["user-input"].value;
+    li.textContent = todo
+    ul.append(li)
+    event.target["user-input"].value = "";
+    
+    li.addEventListener("click", (event)=>{
+        if(event.target.style.textDecoration === "line-through"){
+            event.target.style.textDecoration = ""
+        }
+        else{
+           let line = event.target.style.textDecoration = "line-through"
+           
+       }
+    })
+})
+
+
+
+
