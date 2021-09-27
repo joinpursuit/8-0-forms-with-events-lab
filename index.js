29f4ae2a1221ba88889afd7cd45dbc95e4fa7dac
Nima sherpa <Toplouie78@gmail.com>
2021-09-27 18:33:58 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Code your solution!")
+// creating a form variable 
+let form = document.querySelector("#form");
+
+//form submit EventListener
+form.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    // creating a variable for ul
+    let ul = document.querySelector("ul");
+    // created a li;
+    let li = document.createElement("li");
+
+    li.style.fontSize = 30;
+
+    let inputs = event.target["username"].value;
+
+    li.textContent = inputs;
+    // appending li to ul
+    ul.append(li)
+    //empty the form
+    event.target["username"].value = "";
+
+//li submit EventListener
+    li.addEventListener("click", ()=>{
+        // event.stopPropagation();
+        if(li.style.textDecoration === "line-through"){
+            li.style.textDecoration === "none"
+        }
+        else{
+          li.style.textDecoration = "line-through"
+       }
+    })
+})
