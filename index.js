3a88c7fa4b64cc24bc8e643fc300acfd215f343d
Name Ping Louie <namepinglouie@gmail.com>
2021-09-27 14:50:36 -0400

    did not pass any test but it looks like it was working well

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Code your solution!")
+//console.log("Code your solution!")
+let createUL = document.createElement("ul");
+let selectForm = document.querySelector("form");
+selectForm.after(createUL);
+
+selectForm.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    let task = e.target.todo.value;
+    if(!task) {
+        let p = document.createElement("p");
+        selectForm.after(p);
+        p.textContent = "Task Cannot Be Empty!!!";
+    } else {
+        let alist = document.createElement("li");
+        alist.textContent = task;
+        createUL.append(alist);
+
+        alist.addEventListener("click", (l)=> {
+            if(l.target.style.textDecoration !== "line-through") {
+                l.target.style.textDecoration = "line-through";
+            } else {
+                l.target.style.textDecoration = "none";
+            }
+        })
+    }
+})
