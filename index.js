a35fdc664f81bff08b0943440a672148323161fc
Oliver Chuzan <ochuzan@gmail.com>
2021-09-27 12:04:09 -0600

    Lab Complete w/o Bonus Tasks

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Code your solution!")
+let todoForm = document.querySelector('#todo-item');
+
+todoForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let userList = document.querySelector('ul');
+    let userInput = event.target['list-item'].value;
+    if (!userInput) {
+        let p = document.createElement('p');
+        p.textContent = 'Error. Todo cannot be empty';
+        todoForm.after(p);
+    } else {
+        let listItem = document.createElement('li');
+        listItem.textContent = userInput;
+        userList.append(listItem);
+        event.target['list-item'].value = '';
+    }
+
+    let todoItems = document.querySelectorAll('li');
+
+    for (let li of todoItems) {
+        li.addEventListener('click', () => {
+            li.style.textDecoration = 'line-through';
+        });
+    }
+});
+
