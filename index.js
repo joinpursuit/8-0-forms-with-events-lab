287201f3d6170cbca8bffbd10d197be35351f382
hinkabi <hinkabi@gmail.com>
2021-09-27 22:21:47 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Code your solution!")
+let todoList = document.querySelector("#todo-form");
+
+todoList.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    let todoListValue = event.target["todo-list"].value
+    let ul = document.querySelector("ul");
+    let newLi = document.createElement("li");
+    newLi.textContent = todoListValue
+    ul.append(newLi);
+    event.target["todo-list"].value = ""; 
+
+    let messages = [];
+    if(todoListValue.value === "" || todoListValue === null){
+        messages.push("Error! Todo cannot be empty");
+    }
+
+
+
+    newLi.addEventListener('click', (event) =>{
+        event.target.style.textDecoration = "line-through"; 
+        
+      })
+})
+
+
+
