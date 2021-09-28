0ac98d4ae0d50a3f437e5b90d028bf5bd3d87b5a
BennyZ3 <bennyz3@hotmail.com>
2021-09-28 15:00:53 -0400

    cleaned up a little amd added comments

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  //check if todo contains anything
   if (event.target.todo.value) {
+    //to clear the error msg
     document.querySelector("p").textContent = "";
+    //make a new list item
     const newTodo = document.createElement("li");
     newTodo.textContent = event.target.todo.value;
     newTodo.addEventListener("click", () => {
+      //switch bettween cross out, class toggle is prob easier
       if (newTodo.style.textDecoration !== "line-through") {
         // === 'none' works as well, but fails the test
         newTodo.style.textDecoration = "line-through";
@@ -13,17 +17,17 @@ form.addEventListener("submit", (event) => {
         newTodo.style.textDecoration = "none";
       }
     });
+    //adding a delete button element
     const deleteButton = document.createElement("button");
     deleteButton.addEventListener("click", (eventDel) => {
       newTodo.parentNode.removeChild(newTodo);
     });
-    // deleteButton.style.height = "15px";
-    // deleteButton.style.width = "40px";
     deleteButton.style.marginLeft = "10px";
     deleteButton.textContent = "Delete";
     newTodo.append(deleteButton);
     document.querySelector("ul").append(newTodo);
   } else {
+    //error message for empty input
     document.querySelector("p").textContent = "Error. Todo cannot be empty";
   }
   // event.target.todo.value = "";
