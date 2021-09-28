0c7913a9ecc0e35cd45587836ac8c2808d52c4a5
Xiaoming Yang <xiaomingyang@pursuit.org>
2021-09-27 21:06:26 -0400

    Forms with Events Lab by Xiaoming Yang

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-console.log("CODE YOUR SOLUTION")
+// console.log("CODE YOUR SOLUTION")
 const heading = document.querySelector("h1");
-heading.innerHTML = "My To-Dos";
+heading.innerHTML = "Todos";
 const hr = document.createElement("hr");
 document.body.appendChild(hr);
-const x = document.getElementById("myForm")
-const form = document.createElement("form");
-form.setAttribute("id", "myForm");
-document.body.appendChild(form);
-const y = document.createElement("input");
-y.setAttribute("type", "text");
-y.setAttribute("value", "Type a todo");
-document.getElementById("myForm").appendChild(y);
-const input = document.createElement("INPUT");
-input.setAttribute("type", "submit");
-document.body.appendChild(input);
-const btn = document.createElement("button");
-input.setAttribute("type", "text");
+// const x = document.getElementById("myForm")
+// const form = document.createElement("form");
+// form.setAttribute("id", "myForm");
+// document.body.appendChild(form);
+// const y = document.createElement("input");
+// y.setAttribute("type", "text");
+// y.setAttribute("value", "Type a todo");
+// document.getElementById("myForm").appendChild(y);
+// const input = document.createElement("INPUT");
+// input.setAttribute("type", "submit");
+// document.body.appendChild(input);
+// const btn = document.createElement("button");
+// input.setAttribute("type", "text");
+// input.setAttribute("")
 // const t = document.createTextNode("Submit");
 // btn.appendChild(t);
 // document.body.appendChild(btn);
+
+
