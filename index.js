dd213e69fafe62a097d6471cee42d408da0f1208
Xiaoming Yang <xiaomingyang@pursuit.org>
2021-09-28 10:11:35 -0400

    Forms with Events Lab by Xiaoming Yang

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-// console.log("CODE YOUR SOLUTION")
+const body = document.querySelector("body");
 const heading = document.querySelector("h1");
 heading.innerHTML = "Todos";
 const hr = document.createElement("hr");
 document.body.appendChild(hr);
-const body = document.querySelector("body");
-const form = document.createElement("form");
-const input = document.createElement("INPUT");
 const paragraph = document.createElement("p");
+    paragraph.style.display="none";
+    paragraph.textContent = "Error! Todo cannot be empty";
+const form = document.createElement("form");
 const btn = document.createElement("button");
-const ul = document.createElement("ul");
-input.setAttribute("type", "text");
-input.setAttribute("id", "todolist");
-btn.setAttribute("type", "submit");
-btn.textContent = "Submit";
-document.body.append(paragraph);
-paragraph.style.display="none";
-paragraph.textContent = "Error! Todo cannot be empty";
-document.body.append(form);
-form.append(paragraph);
+    btn.setAttribute("type", "submit");
+    btn.textContent = "Submit";
+    btn.style["margin-left"] = "12px";
+
+const input = document.createElement("INPUT");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "input");
+    input.setAttribute("value", "Type a todo");
+
+body.append(paragraph);
+body.append(form);
+form.append(input);
 form.append(btn);
-document.body.append(ul);
+body.append(document.createElement("ul"));    
+
+
+// const ul = document.createElement("ul");
+
+
+
+// document.body.appendChild(paragraph);
+
+// document.body.appendChild(form);
+// form.append(paragraph);
+// form.append(btn);
+// document.body.appendChild(ul);
 
 // const x = document.getElementById("myForm")
 
@@ -29,14 +43,13 @@ document.body.append(ul);
 // y.setAttribute("type", "text");
 // y.setAttribute("value", "Type a todo");
 // document.getElementById("myForm").appendChild(y);
-// 
+
 // input.setAttribute("type", "submit");
 // document.body.appendChild(input);
-// 
+
 // input.setAttribute("type", "text");
 // input.setAttribute("")
 // const t = document.createTextNode("Submit");
 // btn.appendChild(t);
 // document.body.appendChild(btn);
-
 
