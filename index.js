7d5372090ee20f73aa0fcac82b44796980f1f19c
Lakisha J <lakishajohnson@pursuit.org>
2021-09-28 17:02:37 -0400

    Added p-tag to DOM

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//This function creates an li, adds a textNode to li.
+//This function creates an li then adds a textNode to li.
 //Returns complete list item
 function contactTemplate(name) {
     const li = document.createElement("li");
@@ -28,6 +28,7 @@ input.setAttribute('name', 'userInput')
 input.setAttribute('id', 'name')
 input.setAttribute('type', 'text')
 form.append(input);
+
 //Creates a Button tag, set attributes and give it a textNode
 const submitButn = document.createElement('button');
 submitButn.setAttribute('type', 'submit')
@@ -36,42 +37,46 @@ submitButn.textContent = 'submit'
 form.append(submitButn);
 //Appends whole form to body
 body.append(form);
+
+//Creates a p tag and appends it to body tag
+const paragraph = document.createElement('p');
+body.append(paragraph);
+
 //Creates an ul tag
 const unOrderedList = document.createElement('ul');
 //Appends ul as a sibling of the form tag
-form.after(unOrderedList)
-//Selects the form using its id
+form.after(unOrderedList);
+
+//Selects the form tag by its id
 const formQuery = document.querySelector("#form-content");
-//We are calling EventListener on the form tag w/2 arguments, the type (submit) and a callback (event).
+//We are adding an EventListener on the form tag w/2 arguments, the type(submit) and a callback(event).
 formQuery.addEventListener("submit", (event) => {
-    //Expected to stop the default behavior of pressing submit button
+    //With an expected return to stop the default behavior of pressing submit button
     event.preventDefault();
     //Creates variable to store the value of userInput
-    // console.log(event.target.userInput.value);
     const name = event.target.userInput.value;
-    //Calling above function that selects ul and appends li as a child of unordered list
-    generateContact(name);
-    //Resets the input field of above function
-    event.target.reset()
-});
-//Appends ul tag to form tag
-form.append(unOrderedList);
+    //If input box is empty when button pressed, a message will appear inside p tag
+    if(!name) {
+       paragraph.textContent = "Please enter an item"; 
+    } else { 
+        //Call above function that selects ul and appends li as a child of unordered list
+        generateContact(name);
+        //Resets the input field of above function
+        event.target.reset()}
+    });
+    
+    //Appends ul tag to form tag
+    form.append(unOrderedList);
+    
+    //Selects unordered list to add an EventListener w/2 arguments
+    document.querySelector("ul").addEventListener("click", (event) => {
+        //Expected to stop the default behavior of clicking on ul
+        event.preventDefault();
+        //When target ul is clicked a text-decoration will be applied to that ul
+        event.target.style["text-decoration"] = "line-through";
+      });
+    
 
-//This function creates an li, adds a textNode to li.
-//Returns complete list item
-// function contactTemplate(name) {
-//     const li = document.createElement("li");
-//     li.textContent += name;
-//     return li;
-// }
-
-//Selects unordered list to add an EventListener w/2 arguments
-document.querySelector("ul").addEventListener("click", (event) => {
-    //Expected to stop the default behavior of pressing on ul
-    event.preventDefault();
-    //When target ul is clicked a text-decoration will be applied to that ul
-    event.target.style["text-decoration"] = "line-through";
-  });
 
 
 
