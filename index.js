7db69e0178d572abd57f16a449dcf7f988b935c3
Oliver Chuzan <ochuzan@gmail.com>
2021-09-27 21:16:50 -0600

    Added delete button

--- index.js
+++ index.js
@@ -13,15 +13,45 @@ todoForm.addEventListener('submit', (event) => {
         let listItem = document.createElement('li');
         listItem.textContent = userInput;
         userList.append(listItem);
-        event.target['list-item'].value = '';
+        
+        let deleteButton = document.createElement('button');
+        deleteButton.setAttribute('type', 'submit');
+        deleteButton.textContent = 'Delete';
+        listItem.append(deleteButton);
+        event.target['list-item'].value = "";
+        deleteButton.addEventListener('click', () => {
+            listItem.remove();
+        });
     }
 
     let todoItems = document.querySelectorAll('li');
 
+    // for (let li of todoItems) {
+    //     console.log(li.style.textDecoration)
+    //     li.addEventListener('click', () => {
+    //         li.style.textDecoration = 'line-through';
+    //         // console.log(li.style.textDecoration)
+    //     });
+    // }
+    
+    // for (let li of todoItems){
+    //     if (li.style.textDecoration === 'line-through') {
+    //         li.addEventListener('click', () => {
+    //             li.style.textDecoration = 'none';
+    //         });
+    //     }
+    // }
+
     for (let li of todoItems) {
-        li.addEventListener('click', () => {
-            li.style.textDecoration = 'line-through';
-        });
+        console.log(li.style.textDecoration)
+        if (li.style.textDecoration === '') {
+            li.addEventListener('click', () => {
+                li.style.textDecoration = 'line-through';
+            });
+        } else if (li.style.textDecoration === 'line-through') {
+            li.addEventListener('click', () => {
+                li.style.textDecoration = 'none';
+            });
+        }
     }
 });
-
