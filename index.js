61f11d1e8ec54206a113f3f7898961f37d953b26
salrey <tekdehn@gmail.com>
2021-09-28 02:03:34 -0400

    Add eventlistener for character limit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Code your solution!")
+// Character limit countdown
+const characterLimit = 30;
+const input = document.querySelector("#to-do");
+const count = document.querySelector("#count");
+count.textContent = characterLimit;
+
+input.addEventListener("keydown", (event) => {
+  const length = input.value.length;
+  if (length > characterLimit) {
+    event.preventDefault();
+  } else {
+    count.textContent = characterLimit - length;
+  }
+  event.target.reset();
+});
