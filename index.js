0351f7d9f7afdf074c0f6932282a08b5cd9582da
Jerry De Jesus <jdejesus7@live.com>
2021-09-27 21:02:58 -0400

    completed base lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Code your solution!")
+
+const formSubmission = document.querySelector("#user-form");
+
+//listen for submit button
+formSubmission.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    const ul = document.querySelector("ul");
+    const li = document.createElement("li");
+    li.textContent = event.target["listContents"].value;
+    if(li.textContent === ""){
+        const error = document.createElement("p");
+        error.textContent = "List item must contain characters."
+        formSubmission.append(error);
+    }else{
+        ul.append(li);
+    }
+    
+
+    //listen for line through
+
+    li.addEventListener("click", (event) =>{
+        event.target.style.textDecoration = "line-through";
+    })
+
+})
