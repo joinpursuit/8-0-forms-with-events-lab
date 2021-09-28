f18e04a966150c4690533f428f6eaab875de18b4
JoshMarte <joshuelm@gmail.com>
2021-09-28 17:38:14 -0400

    completed bonus

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
 let userForm = document.querySelector("form");
 let list = document.querySelector("#items");
 
+document.querySelector("textarea").addEventListener("keydown", (event) => {
+  //   console.log(event.which);
+  if (event.which === 13 && !event.shiftKey) {
+    event.preventDefault();
+    userForm.dispatchEvent(new Event("submit"));
+  }
+});
+
 userForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  let item = todo.value;
+
+  function createListItem() {
+    item = item.split("\n");
+
+    for (let thingy of item) {
+      if (thingy.trim() === "") {
+        continue;
+      }
+      let listitem = document.createElement("li");
+      listitem.textContent = thingy;
+
+      listitem.style.margin = "10px";
+
+      let liButton = document.createElement("button");
+      liButton.type = "button";
+      liButton.innerHTML = "delete";
+      liButton.style.margin = "10px";
+      listitem.append(liButton);
+      list.append(listitem);
+
+      listitem.addEventListener("click", (event) => {
+        if (
+          event.target.style.textDecoration === "none" ||
+          event.target.style.textDecoration === ""
+        ) {
+          event.target.style.textDecoration = "line-through solid rgb(0, 0, 0)";
+        } else {
+          event.target.style.textDecoration = "none";
+        }
+      });
+    }
+  }
+
+  let item = textarea.value;
   let ps = document.querySelector("ul p");
   if (!item && !ps) {
     let pTag = document.createElement("p");
@@ -11,36 +52,19 @@ userForm.addEventListener("submit", (event) => {
     list.prepend(pTag);
   } else if (item && ps) {
     ps.remove();
-    let listitem = document.createElement("li");
-    listitem.textContent = item;
-    list.append(listitem);
-
-    listitem.style.margin = "10px";
-    todo.value = "";
+    createListItem();
+    event.target.reset();
+    //textarea.value = "";
   } else if (item && !ps) {
-    let listitem = document.createElement("li");
-    listitem.textContent = item;
-    list.append(listitem);
-
-    listitem.style.margin = "10px";
-    todo.value = "";
+    createListItem();
+    event.target.reset();
+    //textarea.value = "";
   }
 
-  let allItems = document.querySelectorAll("li");
-  for (let liItem of allItems) {
-    liItem.addEventListener("click", (event) => {
-      liItem.style.textDecoration = "line-through solid rgb(0, 0, 0)";
+  let buttonBol = document.querySelectorAll("li button");
+  for (buttons of buttonBol) {
+    buttons.addEventListener("click", (event) => {
+      event.path[1].remove();
     });
   }
 });
-// liButton.addEventListener("click", (event) => {
-//     liItem.remove();
-//   });
-// let buttonBol = document.querySelector("li button");
-
-//     if (!liItem.contains(buttonBol)) {
-//       liButton.type = "button";
-//       liButton.innerHTML = "delete";
-//       liButton.style.margin = "10px";
-//       liItem.append(liButton);
-//     }
