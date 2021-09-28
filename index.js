75e41ba4617ea0a1b7f7affb479cb0615b0801a4
BennyZ3 <bennyz3@hotmail.com>
2021-09-28 11:37:59 -0400

    testing if circle works, cypress is all fails

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Code your solution!")
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (event.target.todo.value) {
+    document.querySelector("p").textContent = "";
+    const newTodo = document.createElement("li");
+    newTodo.textContent = event.target.todo.value;
+    newTodo.addEventListener("click", () => {
+      newTodo.style.textDecoration = "line-through";
+    });
+    // event.target.todo.value = "";
+    form.reset();
+    document.querySelector("ul").append(newTodo);
+  } else {
+    document.querySelector("p").textContent = "Error. Todo cannot be empty";
+    form.reset();
+  }
+});
