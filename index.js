1c21d314b73eb641f052970e9b0f218817110179
BennyZ3 <bennyz3@hotmail.com>
2021-09-28 14:22:12 -0400

    added delete buttons

--- index.js
+++ index.js
@@ -7,11 +7,21 @@ form.addEventListener("submit", (event) => {
     newTodo.textContent = event.target.todo.value;
     newTodo.addEventListener("click", () => {
       if (newTodo.style.textDecoration !== "line-through") {
+        // === 'none' works as well, but fails the test
         newTodo.style.textDecoration = "line-through";
       } else {
         newTodo.style.textDecoration = "none";
       }
     });
+    const deleteButton = document.createElement("button");
+    deleteButton.addEventListener("click", (eventDel) => {
+      newTodo.parentNode.removeChild(newTodo);
+    });
+    // deleteButton.style.height = "15px";
+    // deleteButton.style.width = "40px";
+    deleteButton.style.marginLeft = "10px";
+    deleteButton.textContent = "Delete";
+    newTodo.append(deleteButton);
     document.querySelector("ul").append(newTodo);
   } else {
     document.querySelector("p").textContent = "Error. Todo cannot be empty";
