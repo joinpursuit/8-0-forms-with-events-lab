dc8d4e1d8aeeef28bc1489843a82d33d10fb6182
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2021-09-28 09:33:17 -0400

    Forms with events Lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Code your solution!")
+let userInput = document.querySelector("#user-form");
+    userInput.addEventListener("submit", (event)=>{
+        event.preventDefault()
+            const ul = document.querySelector("ul");
+            const li = document.createElement("li");
+            const list = event.target["user-input"].value;
+                li.textContent = list
+                ul.append(li)
+                event.target["user-input"].value = "";
+
+        li.addEventListener("click", (event)=>{
+            if(event.target.style.textDecoration === "line-through"){
+                event.target.style.textDecoration = ""
+            }
+            else{
+            event.target.style.textDecoration = "line-through"
+        }
+        })
+    })
+
