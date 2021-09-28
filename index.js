aa7a681d5210ac72749ed5f65e6217a973c1aeb4
imolina212 <86175612+imolina212@users.noreply.github.com>
2021-09-28 13:50:58 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Code your solution!")
+const input = document.querySelector("input");
+const form = document.querySelector("form");
+const button = document.querySelector("button");
+
+console.log("hello");
+form.addEventListener("submit", (event) => {
+  console.log("sup");
+  event.preventDefault();
+  const li = document.createElement("li");
+  li.textContent = input.value;
+  document.querySelector("ul").append(li);
+
+  const list = document.querySelectorAll("ul li");
+
+  list.forEach((li) => {
+    li.addEventListener("click", () => {
+      li.setAttribute("style", "text-decoration: line-through");
+    });
+  });
+});
