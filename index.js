422a7b25057c35e6f636cc2f4801a016cbcb74d9
Jenna Qian <jiayin1996@gmail.com>
2021-09-27 21:31:16 -0400

    Attempt at bonus

--- index.js
+++ index.js
@@ -9,16 +9,25 @@ toDoForm.addEventListener("submit", (event) => {
 
   if (enterToDo.value) {
     li.textContent = enterToDo.value;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "Delete"
+    deleteBtn.setAttribute("id", "deleteBut")
+
     ul.append(li);
+    li.append(deleteBtn);
     enterToDo.value = clearInput;
+
+    deleteBtn.addEventListener("click", () => {
+      li.remove();
+      window.alert("You have removed this to-do: " + li.textContent);
+    });
+
+    li.addEventListener("click", (event) => {
+      li.style.textDecoration = "line-through";
+    });
   } else {
     const p = document.createElement("p");
     p.textContent = "Error. Todo cannot be empty!";
     toDoForm.append(p);
   }
-
-  li.addEventListener("click", (event) => {
-    li.style.textDecoration = "line-through";
-    window.alert("You have crossed out this to-do: " + li.textContent);
-  });
 });
