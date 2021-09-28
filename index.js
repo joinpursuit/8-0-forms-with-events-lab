268901a8a2a5226785eb6af57750ca20a132ac03
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2021-09-28 15:00:14 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Code your solution!")
+// console.log("Code your solution!")
+
+const formTag = document.querySelector('form');
+
+formTag.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const userInput = event.target.todo.value;
+    console.log(userInput);
+    const list = document.createElement('li');
+    list.textContent = userInput;
+    
+    const unorderedList = document.querySelector('ul');
+    unorderedList.append(list);
+    
+    const todos = document.querySelectorAll('li');
+    console.log(todos);
+    
+    todos.forEach((todo) => {
+        todo.addEventListener('click', (event) => {
+            todo.style.textDecoration = 'line-through solid';
+        })
+    })
+    
+})
