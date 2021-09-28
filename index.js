1aea4e5140be372fa1c503a6a04b854e8bfcc07d
salrey <tekdehn@gmail.com>
2021-09-28 02:29:14 -0400

    Refactor code

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 // Character limit countdown
-const characterLimit = 30;
 const input = document.querySelector("#to-do");
 const count = document.querySelector("#count");
+const characterLimit = input.maxLength;
 count.textContent = characterLimit;
 
 input.addEventListener("keydown", (event) => {
   const length = input.value.length;
-  if (length > characterLimit) {
-    event.preventDefault();
-  } else {
-    count.textContent = characterLimit - length;
-  }
+  count.textContent = characterLimit - length;
   event.target.reset();
 });
+
+//Add to the list
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  count.textContent = characterLimit;
+
+  const ul = document.querySelector("ul");
+  const li = document.createElement("li");
+  li.textContent = input.value;
+
+  ul.append(li);
+  event.target.reset();
+});
+
+//display error message
