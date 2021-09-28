9f60aef2aab29faaa7dcb0975e9d78721b1b759b
Mukayila ALAO <mukayilaalao@pursuit.org>
2021-09-27 22:46:50 -0400

    Additional part line through or none

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 console.log("Code your solution!")
-let myForm=document.querySelector("#form-id");
+let myForm=document.querySelector("#form-id"); 
+
+
 myForm.addEventListener("submit", e=> {
     e.preventDefault();
     let ul=document.querySelector("ul");
@@ -8,9 +10,42 @@ myForm.addEventListener("submit", e=> {
         li.textContent=e.target.todoslists.value;
         e.target.todoslists.value="";
         ul.append(li);
-        li.addEventListener("click", e=>{
-            e.target.style.textDecoration="line-through";
+
+        // creating delete button
+        let delButton=document.createElement("button");
+        delButton.setAttribute("type", "submit");
+        delButton.setAttribute("name", "submit");
+        delButton.textContent="Delete";
+        li.after(delButton);
+        
+        // function to switch between none and line through.
+        function textDecorationOrNone(changer){
+        
+            if(changer===1) {
+                li.addEventListener("click", e=>{
+                    e.target.style.textDecoration="none"; 
+                    changer=0;
+                    return textDecorationOrNone(changer);
+                });
+            }else {
+                li.addEventListener("click", e=>{
+                    e.target.style.textDecoration="line-through"; 
+                    changer=1;
+                    return textDecorationOrNone(changer);
+                });
+            }   
+        }
+
+        textDecorationOrNone();
+        li.addEventListener("dlclick", (e2Inv)=> {
+            e2Inv.target.style.textDecoration="none";
+
         })
+        delButton.addEventListener("click", ()=> {
+            li.remove();
+            delButton.remove();
+        })
+        // error
     }else {
         let p=document.createElement("p");
         p.textContent="Error:text should not be empty! ";
