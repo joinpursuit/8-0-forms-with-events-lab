7c7bc40c0d15b733940639d1e2b39445f5f42f25
Sara D'Arce <saradarce@gmail.com>
2021-09-27 22:21:34 -0400

    Completed HTML

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-console.log("Code your solution!")
+console.log("Code your solution!");
+toDoList.addEventListener("submit", (e) => {
+  e.preventDefault();
 
+  let li = document.createElement("li");
+
+  let ul = document.querySelector("ul");
+
+  let toDo = e.target["to-do-list"].value;
+
+  li.textContent = toDo;
+
+  ul.append(li);
+
+  e.target["to-do-list"].value = "";
+
+  li.addEventListener("click", (e) => {
+    if (e.target.style.textDecoration === "line-through") {
+      e.target.style.textDecoration = "";
+    } else {
+      let lineItem = (e.target.style.textDecoration = "line-through");
+    }
+  });
+});
