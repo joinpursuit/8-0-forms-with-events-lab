4ed6a10f8443c239515743c8b8726e16e5865a8b
Rohan-Sterling <rohansterling@pursuit.org>
2021-09-28 12:41:13 -0400

    add solutions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Code your solution!")
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const form = document.querySelector('form')
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    console.log(input.value)
+    const li = document.createElement('li')
+    li.textContent = input.value
+    document.querySelector('ul').append(li)
+    
+    const list = document.querySelectorAll('ul li')
+    
+    list.forEach(li =>{
+        li.addEventListener('click', () => {
+            li.setAttribute('style', 'text-decoration: line-through')
+        })
+    })
+})
