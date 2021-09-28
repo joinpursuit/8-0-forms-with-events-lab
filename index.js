afac59f305161a992b6fffbb403d1bbe8379ce31
Angie G <angieg@Angies-MBP.fios-router.home>
2021-09-28 18:46:07 -0400

    Clear Error #3

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Code your solution!")
+
+const form = document.querySelector("#todo");
+form.addEventListener("submit", (event) => {
+event.preventDefault();
+const li = document.createElement('li');
+li.textContent = event.target.input.value; 
+const ul = document.querySelector('ul');
+ul.append(li);
+// event.target.remove();
+})
+
+document.querySelector("ul").addEventListener("click", (event) => {
+event.preventDefault();
+event.target.style["text-decoration"] = "line-through";
+});
+
+
+    
+
