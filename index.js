697312fe4f9491253e2eb5bc75695e0d1e4f82a0
Laiba Sajid <sajid.laiba378@gmail.com>
2021-09-27 21:05:58 -0400

    new list item value appends to unordered list

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Code your solution!")
+let form = document.querySelector("#new-text")
+
+let unordered = document.querySelector("ul")
+
+form.addEventListener("submit", (event)=>{
+    //stops page from refreshing
+    event.preventDefault();
+
+    //create a new list
+    let newLi = document.createElement("li");
+    newLi.textContent = event.target["name-field"].value
+
+    //IGNORE ~~~ FOR MY OWN UNDERSTANDING
+    //adds userInput valuse into "ul" as a list item 
+    unordered.append(newLi)
+    //selects the form tag 
+    console.log("This thing give form", event.target)
+    //selects the submit box
+    console.log("", event.target["name-field"]) 
+    //selects the value of the submit box (.value not .textContent)
+    console.log("trigger", event.target["name-field"].value)
+    //IGNORE ~~~ FOR MY OWN UNDERSTANDING
+})
+
