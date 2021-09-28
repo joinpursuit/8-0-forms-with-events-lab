073f63e3d132d38fd8a2d5d646188a0b09fa3c22
Xiaoming Yang <xiaomingyang@pursuit.org>
2021-09-27 20:36:49 -0400

    Forms with Events Lab by Xiaoming Yang

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Code your solution!")
+console.log("CODE YOUR SOLUTION")
+const heading = document.querySelector("h1");
+heading.innerHTML = "My To-Dos";
+const hr = document.createElement("hr");
+document.body.appendChild(hr);
+const x = document.getElementById("myForm")
+const form = document.createElement("form");
+form.setAttribute("id", "myForm");
+document.body.appendChild(form);
+const y = document.createElement("input");
+y.setAttribute("type", "text");
+y.setAttribute("value", "Type a todo");
+document.getElementById("myForm").appendChild(y);
+const input = document.createElement("INPUT");
+input.setAttribute("type", "submit");
+document.body.appendChild(input);
+const btn = document.createElement("button");
+input.setAttribute("type", "text");
+// const t = document.createTextNode("Submit");
+// btn.appendChild(t);
+// document.body.appendChild(btn);
