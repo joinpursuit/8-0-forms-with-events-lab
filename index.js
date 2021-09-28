42bd0d8f2700d49425f3d62ef73005f314ef9b93
“Lili <lilyhuang114classb@gmail.com>
2021-09-27 23:58:02 -0400

    Passed addtodo_spec tests.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Code your solution!")
+// console.log("Code your solution!")
+let ul = document.createElement("ul");
+let form = document.querySelector("form");
+form.append(ul);
+
+form.addEventListener("submit", (event)=> {
+    event.preventDefault();
+    let userInput = event.target["textarea"].value; 
+    if(!userInput) {
+        let message = document.createElement("p");
+        message.textContent = "User input cannot be empty! Please type again!";
+        form.prepend(message);
+    } else {
+        let list = document.createElement("li");
+        list.textContent = userInput;
+        ul.append(list);
+
+        list.addEventListener("click", (e)=> {
+            e.target.style.textDecoration = "line-through";
+        })
+    }
+})
