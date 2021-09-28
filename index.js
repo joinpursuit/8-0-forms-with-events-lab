40bb5d85b1d2d2600f5b8eca3d1dd9ec2f379abb
Taylor W <taylorwilliams@pursuit.org>
2021-09-28 19:39:12 -0400

    Updates JavaSript File

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Code your solution!")
+const ul = document.querySelector("ul");
+const form = document.querySelector("form");
+
+Paragraph = document.querySelector("p");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const ToDos = event.target.ToDo.value;
+  if (!ToDos) {
+    Paragraph.textContent = "Error, must type text in the text input field";
+  } else {
+    Paragraph.textContent = "";
+    const li = document.createElement("li");
+    
+    const deleteButton = document.createElement('button');
+    deleteButton.setAttribute('type', "submit");
+    deleteButton.textContent = 'Delete';
+    li.append(deleteButton);
+    li.textContent = ToDos;
+    li.style.textDecoration = "line-through none solid rgb(0, 0, 0)";
+    ul.append(li);
+
+    li.addEventListener("click", (event) => {
+      event.target.style.textDecoration = "line-through solid rgb(0, 0, 0)";
+    });
+    form.reset();
+  }
+});
