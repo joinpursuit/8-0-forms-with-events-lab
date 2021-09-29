61ab427d092730adb4b8086402398360f4ad6a0c
salrey <tekdehn@gmail.com>
2021-09-29 02:26:52 -0400

    Add multiple to-do from textarea, update styling, comments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Character limit countdown
+// Setup character limit countdown
 const input = document.querySelector("#to-do");
 const count = document.querySelector("#count");
 const characterLimit = input.maxLength;
@@ -9,56 +9,113 @@ input.addEventListener("keydown", (event) => {
   count.textContent = characterLimit - length;
 });
 
-//Add to the list
+//--------form start---------
+
+//Query form and start by reset error message if any
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
+  // Clear error message upon event
   count.textContent = characterLimit;
   if (document.querySelector("p")) {
     document.querySelector("p").remove();
   }
 
+  //Then add input to the list if there is any
+
+  // NOTE: The \n is invisible in the console; what's shown in the console is not in strings
+  //   if (input.value.includes("\n")) {
+  //     console.log(true);
+  //   }
+
   if (input.value) {
     const ul = document.querySelector("ul");
-    const li = document.createElement("li");
-    li.textContent = input.value;
-    const span = document.createElement("span");
-    span.classList = "trash";
-    span.textContent = "🗑";
-    li.prepend(span);
-    ul.append(li);
 
-    const trash = document.querySelectorAll(".trash");
-    trash.forEach((bin) => {
-      bin.addEventListener("click", (event) => {
-        event.target.nextSibling.remove();
-        bin.remove();
+    //Add multiple lines if any
+    if (input.value.includes("\n")) {
+      const lines = input.value.split("\n");
+      for (let line of lines) {
+        let li = document.createElement("li");
+        li.textContent = line;
+
+        // Add delete button to each list item
+        const button = document.createElement("button");
+        button.classList = "trash";
+        button.type = "button";
+        button.textContent = "🗑";
+        button.style.backgroundColor = "white";
+
+        //Update DOM
+        li.prepend(button);
+        ul.append(li);
+
+        //Add event to target and TOGGLE cross out text decoration
+        li.addEventListener("click", (event) => {
+          if (!event.target.style.textDecoration.includes("line-through")) {
+            event.target.style.textDecoration = "line-through solid black";
+          } else {
+            event.target.style.textDecoration = "none solid rgb(0, 0, 0)";
+          }
+          // OR TOGGLE style text via CSS classList /JS
+          //   event.target.classList.toggle("cross-text");
+        });
+
+        //Remove list upon event
+        button.addEventListener("click", (event) => {
+          event.target.parentElement.remove();
+        });
+
+        // Clear & start over upon event
+        event.target.reset();
+      }
+    } else {
+      //Or Add single lines of input if any
+      li = document.createElement("li");
+      li.textContent = input.value;
+
+      //Add delete button to each list item
+      const button = document.createElement("button");
+      button.classList = "trash";
+      button.type = "button";
+      button.textContent = "🗑";
+      button.style.backgroundColor = "white";
+
+      // Update DOM
+      li.prepend(button);
+      ul.append(li);
+
+      //Add event to target and TOGGLE cross out text decoration
+      li.addEventListener("click", (event) => {
+        if (!event.target.style.textDecoration.includes("line-through")) {
+          event.target.style.textDecoration = "line-through solid black";
+        } else {
+          event.target.style.textDecoration = "none solid rgb(0, 0, 0)";
+        }
+        // OR TOGGLE style text via CSS classList /JS
+        //   event.target.classList.toggle("cross-text");
       });
-    });
 
-    //Add text decoration to cross out items
-    li.addEventListener("click", () => {
-      li.style.textDecoration = "line-through solid black";
-    });
+      //Remove list upon event
+      button.addEventListener("click", (event) => {
+        event.target.parentElement.remove();
+      });
 
-    event.target.reset();
+      // Clear & start over upon event
+      event.target.reset();
+    }
   } else {
-    //display error message
+    //If no input, then display error message
     if (!document.querySelector("p")) {
       const error = document.createElement("p");
       error.textContent = "Try again! There's nothing to add.";
+      // Update DOM
       form.append(error);
+      // Clear & start over upon event
       event.target.reset();
     }
   }
 });
 
-//Add text decoration to cross out items
-const lists = document.querySelectorAll("li");
-
-for (let list of lists) {
-  list.addEventListener("click", () => {
-    list.style.textDecoration = "line-through solid black";
-  });
-}
+//------form end-----
