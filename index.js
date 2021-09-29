f2c030a283463638b58356bf8063fcf864d78ec5
kisublee <yigi2021@gmail.com>
2021-09-29 15:14:24 -0400

    Complete bonus specs.

--- index.js
+++ index.js
@@ -10,11 +10,21 @@ const form = document.querySelector("#to-dos")
 form.addEventListener('submit', (event) => {
     //3. prevent the page from refreshing
     event.preventDefault();
-
+    console.log(event);
 
     const list = event.target.list.value;
+    
+
 
     addToList(list);
+
+    if (list.includes("\n")) {
+        const splitList = list.split("\n");
+        const sliceList = splitList.slice(1)
+        sliceList.forEach((each) => {
+            addToList(each)
+        })
+     } 
 
 
     event.target.reset();
@@ -50,8 +60,12 @@ function listTemplate (thing) {
 
 
     return li
-
 }
+
+// Add the ability to add multiple to-dos 
+// if the user submits a text input with multiple lines. Each line should be a new to-do.
+
+
 
 function addToList (thing) {
     
@@ -103,107 +117,4 @@ document.body.append(createDarkModeButton);
 createDarkModeButton.addEventListener("click", () => {
     const body = document.body;
     body.classList.toggle("dark-mode");
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     // const trash = document.querySelector(".garbage");
-//     const ul = document.querySelector("ul");
-//     function eatPasta (input) {
-//         const li = document.createElement("li")
-//         li.textContent += input;
-//         ul.append(li);
-//         const list = document.querySelectorAll("li")
-//         list.forEach((li) => {
-//             li.addEventListener("click",() =>
-//             li.style.textDecoration = "line-through")
-//         })
-        
-//         list.forEach((li) => {
-//             li.addEventListener("dblclick",() =>
-//             li.style.textDecoration = "none solid rgb(0, 0, 0)")
-//         })
-
-//         // const garbage = document.createElement("button");
-//         // garbage.innerHTML = "Trash";
-//         // ul.appendChild(garbage);
-        
-//         // garbage.addEventListener("click", () => {
-//         //     list.remove()
-//         // })
-
-//         // return li
-//     }
-
-
-
-
-// const form = document.querySelector("form");
-
-
-// form.addEventListener("submit", (event) => {
-
-// event.preventDefault();
-
-// const getPasta = event.target.box.value;
-
-// eatPasta(getPasta);
-
-// event.target.reset();
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
