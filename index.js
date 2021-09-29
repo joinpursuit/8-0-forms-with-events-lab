80024f4c8ea51640ba2d7c6c04ebae3ad55bd7ae
caveman49 <caveman4949@gmail.com>
2021-09-29 00:27:08 -0400

    updated_index_js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Code your solution!")
+// const addToDoButton = document.getElementById("submitButton");
+const selectForm = document.querySelector("form");
+const toDoCon = document.getElementById("container");
+
+selectForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const listItem = document.createElement("li");
+  const inputField = event.target.userInput.value;
+  listItem.textContent = inputField;
+  toDoCon.append(listItem);
+});
+
+// todos.forEach((todo) => {
+//     todo.addEventListener("click", (event) => {
+//       todo.style.textDecoration = "line-through solid";
+//     });
+
+toDoCon.addEventListener("click", (event) => {
+  event.preventDefault();
+  const lists = document.querySelectorAll("li");
+  lists.forEach((list) => {
+    event.target.style.textDecoration = "line-through solid";
+  });
+});
