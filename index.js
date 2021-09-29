76a928d5347b10db361848e100500a7b4f315343
stephanie lucano <stephaniemarielucano@gmail.com>
2021-09-28 21:20:17 -0400

    Create list for My To-Dos

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Code your solution!")
+const form = document.querySelector("form")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const item = event.target.item.value
+
+    const unorderedList = document.querySelector("ul")
+    const list = document.createElement("li")
+    list.textContent = item
+    unorderedList.append(list)
+
+    const listItem = document.querySelectorAll("li")
+    listItem.forEach((item) => {
+        item.addEventListener("click", () => {
+            item.style.textDecoration = 'line-through solid rgb(0, 0, 0)'
+        })
+    })
+})
