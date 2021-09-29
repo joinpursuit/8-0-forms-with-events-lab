c01c09fdd59aa6758a62ac7fe9e0f43e3c081a4e
Younes Tahiri <you@example.com>
2021-09-28 20:50:04 -0400

    Did forms-events-lab with Taylor Williams

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Code your solution!")
+const ul = document.querySelector("ul");
+const form = document.querySelector("form");
+Paragraph = document.querySelector("p");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const ToDos = event.target.ToDo.value;
+
+  if (!ToDos) {
+    Paragraph.textContent = "Error, must type text in the text input field";
+  } else {
+    Paragraph.textContent = "";
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute("type", "button");
+    deleteButton.textContent = "Delete";
+    const li = document.createElement("li");
+    li.style.textDecoration = "line-through none solid rgb(0, 0, 0)";
+    li.append(deleteButton, ToDos);
+    ul.append(li);
+
+    li.addEventListener("click", (event) => {
+      event.target.classList.toggle("lineThrough");
+    });
+
+    deleteButton.addEventListener("click", (event) => {
+      li.parentNode.removeChild(li);
+      event.target.parentNode.removeChild(event.target);
+    });
+
+    form.reset();
+  }
+});
