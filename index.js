d4df6a51fa44c08976a746c589a34283c1de21dd
Fatema Kabir <fatema.kabir@pursuit.org>
2021-09-28 23:46:04 -0400

    add-to-do-list

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Code your solution!")
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const paragraph = document.querySelector("p");
+    const ulList = document.querySelector("ul");
+    const list = document.createElement("li");
+    const item = event.target.userInput.value;
+    if (item.length === 0){
+        paragraph.textContent = "Error. Input can not be empty"
+    } else { (item.textContent === "") 
+        list.textContent = item;
+        ulList.append(list);
+    }
+    event.target.reset();
+});
+
+  
+const ulList = document.querySelector("ul");
+    ulList.addEventListener("click", (event) => {
+        event.preventDefault();
+        event.target.style["text-decoration"] = "line-through";
+});
+
+
+
+
+
