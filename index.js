5ef4005ac6ac1815e39dffef163ac2822d341d49
Billy Dominique <billydominique@pursuit.org>
2021-09-29 00:36:44 -0400

    passed the required test

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Code your solution!")
+const selectForm = document.querySelector("form");
+const toDoCon = document.getElementById("container");
+
+selectForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const listItem = document.createElement("li");
+  const inputField = event.target.userInput.value;
+  listItem.textContent = inputField;
+  toDoCon.append(listItem);
+});
+
+toDoCon.addEventListener("click", (event) => {
+  event.preventDefault();
+  const lists = document.querySelectorAll("li");
+  lists.forEach((list) => {
+    event.target.style.textDecoration = "line-through solid";
+  });
+});
