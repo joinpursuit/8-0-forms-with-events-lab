43c1bcf9cecb69fc85a611b67be46de360a7b4f3
Taylor W <taylorwilliams@pursuit.org>
2021-09-28 23:18:25 -0400

    Updated JavaScript File

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const ul = document.querySelector("ul");
 const form = document.querySelector("form");
-
 Paragraph = document.querySelector("p");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -9,18 +8,19 @@ form.addEventListener("submit", (event) => {
     Paragraph.textContent = "Error, must type text in the text input field";
   } else {
     Paragraph.textContent = "";
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute("type", "button");
+    deleteButton.textContent = "Delete";
     const li = document.createElement("li");
-    
-    const deleteButton = document.createElement('button');
-    deleteButton.setAttribute('type', "submit");
-    deleteButton.textContent = 'Delete';
-    li.append(deleteButton);
-    li.textContent = ToDos;
     li.style.textDecoration = "line-through none solid rgb(0, 0, 0)";
+    li.append(deleteButton, ToDos);
     ul.append(li);
-
     li.addEventListener("click", (event) => {
-      event.target.style.textDecoration = "line-through solid rgb(0, 0, 0)";
+      event.target.classList.toggle("lineThrough");
+    });
+    deleteButton.addEventListener("click", (event) => {
+      li.parentNode.removeChild(li);
+      event.target.parentNode.removeChild(event.target);
     });
     form.reset();
   }
