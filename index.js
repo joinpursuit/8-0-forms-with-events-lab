0f87e989a77aea6f2812fbaeb756c8466b146eed
Larisa <larisashapkinab@gmail.com>
2021-09-29 22:21:58 -0400

    8-0-forms-events-lab-

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Code your solution!")
+let form = document.querySelector("form");
+let newList = document.createElement("li");
+let listTask = document.querySelector("ul");
+listTask.append(newList);
+
+    form.addEventListener("submit", (event)=>{
+        event.preventDefault();
+        let userInput = event.target["to-do-list"].value;
+        newList.textContent = userInput;
+
+        if(!userInput){
+            let paragraph = document.createElement("p");
+            form.after(paragraph);
+            paragraph.textContent = "Error.Todo cannot be empty";
+        }
+            newList.addEventListener("click", (event) =>{
+            if(!event.target.style.textDecoration) {
+                event.target.style.textDecoration= "line-through";
+            }else{
+                event.target.style.textDecoration = "none";
+            }
+        })
+})
+
+
