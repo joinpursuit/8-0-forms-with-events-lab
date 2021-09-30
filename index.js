539682fa510b7c2840f1fd5894c145e61f535640
Larisa <larisashapkinab@gmail.com>
2021-09-30 16:50:34 -0400

    Second try

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-let form = document.querySelector("form");
-let newList = document.createElement("li");
-let listTask = document.querySelector("ul");
-listTask.append(newList);
+let form = document.querySelector("#my-do-list");
+
 
     form.addEventListener("submit", (event)=>{
         event.preventDefault();
+    
+        let newList = document.createElement("li");
+        let listTask = document.querySelector("ul");
         let userInput = event.target["to-do-list"].value;
-        newList.textContent = userInput;
+            newList.textContent = userInput
+            listTask.append(newList)
+            event.target["to-do-list"].value = "";
+    
+        // if(!userInput){
+        //     let paragraph = document.createElement("p");
+        //     form.after(paragraph);
+        //     paragraph.textContent = "Error.Todo cannot be empty";
 
-        if(!userInput){
-            let paragraph = document.createElement("p");
-            form.after(paragraph);
-            paragraph.textContent = "Error.Todo cannot be empty";
-        }
             newList.addEventListener("click", (event) =>{
-            if(!event.target.style.textDecoration) {
-                event.target.style.textDecoration= "line-through";
+            if(event.target.style.textDecoration === "line-through") {
+                event.target.style.textDecoration = "";
             }else{
-                event.target.style.textDecoration = "none";
+                event.target.style.textDecoration = "line-through";
             }
         })
 })
+
 
 
