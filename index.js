4abb20a795afcccae444a407ef51b2b98b575529
ERIAN CABALLERO <eriancaballero@ERIANs-MBP.home>
2021-09-29 20:34:40 -0400

    Forms with Event Lab Submission

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Code your solution!")
+// console.log("Code your solution!")
+
+let newForm = document.querySelector("form");
+let newList = document.querySelector("ul");
+
+newForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let addToDoText = e.target["to-do"].value;
+if(!addToDoText){
+    let para = document.createElement("p");
+    newForm.after(para);
+    para.textContent = "Error. Todo cannot be empty"
+}else{
+    let list = document.createElement("li");
+    list.textContent = addToDoText;
+    newList.append(list);
+    list.addEventListener("click", (e)=>{
+        e.target.style.textDecoration = "line-through"
+    });
+}
+
+});
