3f4a86ba2dabc8e080f7d383a6c11b41a45699c4
Derek <plutus.derekpeterson@gmail.com>
2022-03-30 19:49:41 -0400

    Form with events lab completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Code your solution!")
+//console.log("Code your solution!")
+
+//Passed all tests:
+
+let form = document.querySelector('form');
+let ul = document.querySelector('ul');
+let paragraph = document.querySelector('p');
+let text = document.querySelector("input[type='text']");
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (!text.value) {
+    paragraph.textContent = 'Error! Please fill in to-do task.';
+  }
+
+  const li = document.createElement('li');
+  li.textContent = text.value;
+  ul.append(li);
+
+  li.addEventListener('click', () => {
+    li.style.textDecoration = 'line-through';
+  });
+});
+
