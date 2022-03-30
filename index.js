b18f318ba08bbd3646ca6dbfdea9c6b66dd96820
lchen6283 <lchen6283@gmail.com>
2022-03-30 16:22:49 -0400

    forms-events-lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Code your solution!")
+const text = document.querySelector("input[type='text']");
+const uList = document.querySelector("ul");
+const form = document.querySelector("form");
+let p = document.querySelector("p");
+
+form.addEventListener("submit", (todo) => {
+  todo.preventDefault();
+
+  if (text.value === "") {
+    p.textContent = "Error. Todo cannnot be empty";
+  }
+
+  let lists = document.createElement("li");
+  lists.textContent = text.value;
+  uList.append(lists);
+
+  lists.addEventListener("click", () => {
+    lists.style.textDecoration = "line-through";
+  });
+});
