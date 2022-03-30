ddfb7f31993ae189a71fd00718318e70dae8faa0
Stephanie Frias <steph.frias01@me.com>
2022-03-30 19:35:18 -0400

    think im done?

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("Code your solution!")
+
+let form = document.getElementById("to-do");
+let list = document.querySelector("ul");
+//let input = document.getElementById("text");
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); //prevent reload on submit
+  if (!event.target.value) {
+    let error = document.createElement("p");
+    error.textContent = "Error";
+    console.log(error);
+  }
+  let newItem = document.createElement("li");
+  newItem.textContent = event.target.text.value;
+  list.append(newItem);
+  list.addEventListener("click", (event) => {
+    event.target.style.textDecoration = "line-through";
+  });
+});
+
