fcb337b983b13c3fa262e0548542cf3f35fc9bef
bowersjuan <bowersjuan@gmail.com>
2022-03-30 16:47:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Code your solution!")
+const todosList = document.querySelector("ul");
+
+const submitForm = document.querySelector("form");
+
+submitForm.addEventListener("submit", (event) => {
+  let submittedTodo = event.target.todo.value;
+
+  if (!submittedTodo) {
+    let error = document.getElementById("error");
+    error.textContent = "Error. Todo cannot be empty";
+  } else {
+    error.textContent = "";
+    let todoListItem = document.createElement("li");
+    todoListItem.textContent = submittedTodo;
+    todosList.append(todoListItem);
+
+    if (todoListItem) {
+      let allListItems = document.querySelectorAll("li");
+      for (let listItem of allListItems) {
+        listItem.addEventListener("click", (event) => {
+          listItem.setAttribute("style", "text-decoration: line-through;");
+        });
+      }
+    }
+  }
+  event.preventDefault();
+});
