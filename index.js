4bf22dd0f315703f524f430f89bf37c56659372a
lily08 <liliangelxd@gmail.com>
2022-03-30 16:23:43 -0400

    forms with event lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Code your solution!")
+const form = document.querySelector('form')
+const text = document.querySelector("input[type='text']")
+let ul = document.querySelector('ul')
+let p = document.querySelector('p')
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  if (text.value === '') {
+    p.textContent = 'Error. Todo cannnot be empty'
+  }
+
+  let li = document.createElement('li')
+
+  li.textContent = text.value
+
+  ul.append(li)
+
+  li.addEventListener('click', () => {
+    li.style.textDecoration = 'line-through'
+  })
+})
