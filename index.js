eacde003ad1634c19668af0ad77a19dae2b32cea
TaryneLeach <taryneleach@gmail.com>
2022-03-30 12:57:08 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Code your solution!")
+/** @format */
+
+console.log('Code your solution!');
+
+const form = document.querySelector('form');
+const toDo = document.querySelector('ul');
+
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+
+	const li = document.createElement('li');
+	const input = event.target.toDoList.value;
+	li.textContent = input;
+	toDo.append(li);
+
+	toDo.addEventListener('click', (event) => {
+		const list = document.querySelectorAll('li');
+		list.forEach((list) => {
+			event.target.style.textDecoration = 'line-through solid';
+		});
+	});
+});
