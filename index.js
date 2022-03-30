0b3d3e8a9989659fb94e8b710c1838ae56f4906f
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2022-03-30 12:41:24 -0400

    completed forms with events lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Code your solution!")
+const form = document.getElementById("to-do-list");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const inputItem = event.target.addToDo.value;
+  const unorderedList = document.querySelector("ul");
+  const listItem = document.createElement("li");
+
+  listItem.textContent = inputItem;
+  unorderedList.append(listItem);
+
+  listItem.addEventListener("click", (event) => {
+    event.target.style.textDecoration = "line-through";
+  });
+});
