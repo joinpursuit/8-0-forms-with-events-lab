6362648d437fe87a4dabf52fe369490403acc5b3
Antonio Shivers <antonioshivers2500@yahoo.com>
2022-03-30 15:12:20 -0400

    completed main part of lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("Code your solution!")
+const toDo = document.getElementById("toDo");
+const toDoList = document.getElementById("toDoList");
+const errorMessage = document.getElementById("errorMessage");
+const addItem = document.getElementById("addItem");
+addItem.style.height = "25px";
+addItem.style.backgroundColor = "black";
+addItem.style.color = "white";
+
+toDo.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let { inputItem } = e.target;
+  const listItem = document.createElement("li");
+
+  if (inputItem.value === "") {
+    errorMessage.textContent = `ERROR: You're trying to submit an empty value!!!`;
+    setTimeout(() => {
+      errorMessage.textContent = "";
+    }, 3000);
+  } else {
+    const deleteItem = document.createElement("button");
+    deleteItem.textContent = "DELETE";
+    deleteItem.style.backgroundColor = "black";
+    deleteItem.style.color = "red";
+    deleteItem.style.marginLeft = "1%";
+
+    listItem.textContent = inputItem.value;
+    toDoList.append(listItem);
+    listItem.append(deleteItem);
+    inputItem.value = "";
+
+    listItem.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      listItem.style.textDecoration = "line-through";
+    });
+
+    deleteItem.addEventListener("click", (e) => {
+      e.preventDefault();
+    });
+  }
+});
