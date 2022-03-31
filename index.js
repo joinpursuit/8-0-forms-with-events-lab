3220b1b789ff90c9d7e84c4ede07b1f1cd1ba0fe
Andres Varon <anvaron@gmail.com>
2022-03-31 10:13:42 -0400

    DOM-Form-Events lab - Final release

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const list = document.querySelector('#list');
-let listActive = false;
-console.log(list.hasChildNodes())
 // Adding new task to the list
 const form = document.getElementById('new-task');
 form.addEventListener('submit', (e) => {
@@ -9,11 +7,7 @@ form.addEventListener('submit', (e) => {
     const task = document.querySelector('textarea');
     li.appendChild(document.createTextNode(task.value));
     list.append(li)
-    listActive = true;
     task.value = '';
-    console.log(listActive)
-    console.log(list.hasChildNodes())
-    const tasks = document.querySelectorAll('#list li')
 
     li.addEventListener("click", () => {
         li.setAttribute(`style`, `text-decoration: line-through`)
