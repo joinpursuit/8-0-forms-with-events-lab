b5ab9f0c79bca4ae3209557c0bae2868c859c9f6
Edwin Codrington <edwincodrington@gmail.com>
2022-03-31 03:28:01 -0400

    base lab completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Code your solution!")
+//console.log("Code your solution!");
+
+const body = document.querySelector("body");
+const form = document.querySelector("form");
+const list = document.querySelector("ul");
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	let newItem = document.createElement("li");
+	if (!event.target.value) {
+		let errorMsg = document.createElement("p");
+		errorMsg.textContent = "Error: nothing written!";
+		form.append(errorMsg);
+	}
+	newItem.textContent = `${event.target.text.value}`;
+	list.append(newItem);
+	list.addEventListener("click", (event) => {
+		event.target.style = "text-decoration: line-through";
+	});
+});
