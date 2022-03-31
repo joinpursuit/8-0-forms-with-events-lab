36f16bcd126e617ed01efd4920de3ff2fce04fc2
Matt M <Afrodesiacc@gmail.com>
2022-03-30 20:57:35 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log("Code your solution!")
+
+let input = document.querySelector("input")
+let form = document.querySelector("form");
+let unorderedList = document.querySelector("ul");
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+//find text
+//create new li
+//add text to li
+//add li to ul
+let text = input.value 
+let listItem = document.createElement("li");
+
+listItem.innerText = text
+unorderedList.append(listItem)
+
+listItem.addEventListener("click", (event) => {
+    listItem.style.textDecoration = "line-through"
+})
+})
