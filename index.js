bc62c3c605583c52d2cff29433e6e6c0c7e55796
Andres Varon <anvaron@gmail.com>
2022-03-31 10:10:40 -0400

    DOM-Form-Events lab - Final release

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
-const text = document.querySelector("input[type='text']");
-const tasks = document.querySelector("ul");
-const form = document.querySelector("form");
-const p = document.querySelector("p");
+const list = document.querySelector('#list');
+let listActive = false;
+console.log(list.hasChildNodes())
+// Adding new task to the list
+const form = document.getElementById('new-task');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const li = document.createElement("li");
+    const task = document.querySelector('textarea');
+    li.appendChild(document.createTextNode(task.value));
+    list.append(li)
+    listActive = true;
+    task.value = '';
+    console.log(listActive)
+    console.log(list.hasChildNodes())
+    const tasks = document.querySelectorAll('#list li')
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const task = document.createElement("li");
-
-  if (!text.value) {
-    p.textContent = "No dream entered. Try again!";
-  } else {
-    task.textContent = text.value;
-    tasks.append(task);
-  }
-
-  // Bonus: Have the input go back to empty after adding a new todo.
-  document.getElementById("todo").value = "";
-
-  task.addEventListener("click", () => {
-    task.style.textDecoration = "line-through";
-  });
-
-  task.addEventListener("mouseover", () => {
-    task.style.fontWeight = "bold";
-  });
-
-  task.addEventListener("mouseleave", () => {
-    task.style.fontWeight = "normal";
-  });
-});
+    li.addEventListener("click", () => {
+        li.setAttribute(`style`, `text-decoration: line-through`)
+    });
+}) 
