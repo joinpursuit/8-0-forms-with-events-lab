e8a3988899e3b0214e8de367159d1a962f3adec5
arupay <arupay11374@gmail.com>
2022-03-31 09:38:45 -0400

    completed all main tasks. bonus still left to do

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Code your solution!")
+console.log("Code your solution!");
+const ul = document.querySelector("ul");
+const newTodo = document.getElementById("new-todo");
+
+newTodo.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (!event.target.todo.value) {
+    let p = document.createElement("p");
+    p.textContent = "Error todo item cannot be empty.";
+    newTodo.append(p);
+  } else {
+    let li = document.createElement("li");
+    li.textContent = event.target.todo.value;
+    ul.append(li);
+    li.addEventListener("click", () => {
+      li.style.textDecoration = "line-through";
+    });
+  }
+});
