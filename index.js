840a2603622db0f01493083202d3d35cf5ff7b79
named.josie <named.josie@gmail.com>
2022-03-31 08:47:12 -0400

    Finished-forms-with-events Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("Code your solution!")
+const form = document.getElementById("to-do-list");
+form.addEventListener('submit' , (event) => {
+    event.preventDefault()
+   
+    let inputItem = event.target.toDo.value;
+    let unList = document.querySelector(`ul`);
+    let listItem = document.createElement(`li`);
+
+    listItem.textContent = inputItem;
+  unList.append(listItem);
+  listItem.addEventListener(`click`, (event) => {
+    event.target.style.textDecoration = "line-through";
+  });
+} )
