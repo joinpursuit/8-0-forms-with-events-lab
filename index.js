ed9a1bceafe61f07c9df87fe0745bd38a4bceb50
Fig <bethanytrr@gmail.com>
2022-03-31 02:44:49 -0400

    Lab Completed - love that 3AM grind

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Code your solution!")
+console.log('Code your solution!');
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const list = document.querySelector('ul');
+
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+
+	const text = input.value;
+	const submitted = document.createElement('li');
+	submitted.textContent = text;
+	list.append(submitted);
+
+	list.addEventListener('click', (event) => {
+		submitted.style.textDecoration = 'line-through solid';
+	});
+});
