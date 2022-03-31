2b5ac66a82e3027a8978a13337019b80b944d266
Andres Varon <anvaron@gmail.com>
2022-03-31 08:15:39 -0400

    test-03: template tested

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const list = document.querySelector('#list');
-const li = document.createElement("li")
-li.appendChild(document.createTextNode("Wash dishes"));
-list.append(li)
+const list = document.querySelector('#list'),
+      li   = document.createElement("li");
+// Adding new task to the liat
+const form = document.getElementById('new-task');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const task = document.querySelector('textarea').value;
+    li.appendChild(document.createTextNode(task));
+    list.append(li)
+}) 
