34a53717d8d9c3440997ede4eccb60cedbcc7c90
Isaac Gonzalez <u1337x@gmail.com>
2022-03-31 09:04:09 -0400

    Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Code your solution!")
+const form = document.getElementById("To-Do List");
+let list = document.querySelector("ul");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //   if (!event.target.value) {
+  //     throw "Missing fields.";
+  //   }
+
+  let newList = document.createElement("li");
+  newList.textContent = event.target.text.value;
+  list.append(newList);
+  list.addEventListener("click", (event) => {
+    event.target.style.textDecoration = "line-through";
+  });
+});
