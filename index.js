e4d3d3525fa0b5007781be98057d78395acddbfd
Jonathan Scheiber <jon@scheiber.me>
2022-03-30 22:03:22 -0400

    add event listeners

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Code your solution!")
+const text = document.querySelector("input[type='text']");
+const tasks = document.querySelector("ul");
+const form = document.querySelector("form");
+const p = document.querySelector("p");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const task = document.createElement("li");
+
+  if (!text.value) {
+    p.textContent = "No dream entered. Try again!";
+  } else {
+    task.textContent = text.value;
+    tasks.append(task);
+  }
+
+  // Bonus: Have the input go back to empty after adding a new todo.
+  document.getElementById("todo").value = "";
+
+  task.addEventListener("click", () => {
+    task.style.textDecoration = "line-through";
+    task.style.color = "rgb(200, 8, 0)";
+  });
+
+  task.addEventListener("mouseover", () => {
+    task.style.fontWeight = "bold";
+  });
+
+  task.addEventListener("mouseleave", () => {
+    task.style.fontWeight = "normal";
+  });
+});
