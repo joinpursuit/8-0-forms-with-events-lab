7a38e1ed1033df6a7a11c8e0b580a2c450ea38d4
Victor E Lopez <velbx51594@macbook-air.myfiosgateway.com>
2022-04-02 15:04:48 -0400

    f w e lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Code your solution!")
+const ul = document.querySelector('ul')
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const li = document.createElement('li')
+    li.textContent = event.target.todo.value;
+    ul.append(li);
+    
+    li.addEventListener("click", (event) => {
+       li.style.textDecoration = "line-through"
+    });
+    
+    form.reset();
+});
+
+
+
