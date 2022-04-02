c9ffdb9e13a844202d37091d1e53ee112d91204d
Samantha Figueroa <samanthafigueroa@pursuit.org>
2022-04-02 19:03:50 -0400

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("Code your solution!")
+const form = document.querySelector('form');
+
+function createList(todo){
+    const li = document.createElement('li');
+    li.textContent = todo;
+    li.append(document.createElement('br'));
+    return li;
+}
+
+function generateList(todo){
+    const li = createList(todo);
+    const ul = document.querySelector('ul');
+    ul.append(li);
+
+    li.addEventListener("click", (event) => {
+        event.target.style.textDecoration = 'line-through';
+    })
+}
+
+const p = document.querySelector('p');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if(!todo.value){
+        p.textContent = 'Error! Todo cannot be empty';
+        form.append(p);
+    } else{
+        generateList(todo.value);
+    }
+
+    form.reset();
+})
+
+
+
+
+
+
