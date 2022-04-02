f574338a00450de3f8f985b4bbda32b71c8f268e
sabrina <sabrinaescobarflores@gmail.com>
2022-04-02 18:03:03 -0400

    forms and events

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("Code your solution!")
+const ul = document.querySelector('ul')
+const form = document.querySelector('#form1')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const li = document.createElement('li')
+    li.textContent = event.target.myText.value;
+    ul.append(li);
+
+    li.addEventListener("click", (event) => {
+       li.style.textDecoration = "line-through"
+    });
+
+    form.reset();
+});
