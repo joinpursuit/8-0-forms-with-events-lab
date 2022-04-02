2a7e53c6611a91ec49f82e00b9fde0d64a1b4654
masmei <masonmei@pursuit.org>
2022-04-02 16:39:58 -0500

    finished lab :)

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Code your solution!")
+function toDoList(toDo) {
+    const li = document.createElement("li");
+    li.textContent += toDo;
+    return li;
+}
+function generateToDo(toDo) {
+    let li = toDoList(toDo);
+    const ul = document.querySelector("ul");
+    const del = document.createElement("button")
+    del.textContent = "Delete";
+    ul.append(li);
+    li.append(del)
+
+    li.addEventListener('click', (event) =>{
+    li.setAttribute("style", "text-decoration: line-through solid rgb(0, 0, 0)")
+    })
+
+}
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+    // console.log(event.target)
+  event.preventDefault();
+generateToDo(event.target.toDo.value)
+form.reset();
+})
+
+
