1ddf666b82d4352ca76eb0e4c60a31b8c42e6603
Christine Taylor <ctay47@icloud.com>
2022-04-02 17:36:59 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log("Code your solution!")
+
+
+const form = document.querySelector('#todo')
+
+form.addEventListener('submit',(event) =>{
+    event.preventDefault()
+
+    const ul = document.querySelector('ul')
+    const li = document.createElement('li')
+    li.textContent = event.target.list.value
+    if(li.textContent === ""){
+        const error = document.createElement('p')
+        error.textContent = 'Error. Todo cannot be empty'
+        ul.before(error)
+    }
+    ul.append(li)
+
+    li.addEventListener('click',(event) => {
+        event.target.style.textDecoration='line-through'
+    })
+    
+    
+})
