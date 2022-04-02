701ae735f1219db09a67037f0e43de5e3dcd9161
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-04-02 14:43:23 -0400

    finished basic lab requirements

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Code your solution!")
+
+
+const form = document.querySelector("#inputForm");
+const todoList = document.querySelector("#todoList");
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let text = event.target.textInput.value;
+    if (!text) {
+        document.getElementById("errorMessage").textContent = "Error: Nothing to do!"
+    } else {
+        document.getElementById("errorMessage").textContent = "";
+        let newTodo = document.createElement("li");
+        newTodo.textContent = text;
+        todoList.append(newTodo);
+        form.reset();
+    }
+});
+
+todoList.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log (e.target.style);
+    e.target.style.textDecoration = 'line-through';
+});
