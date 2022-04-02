033d59b3f085363758fc940a4b7a806ef972c63b
Andres Varon <anvaron@gmail.com>
2022-04-02 12:10:02 -0400

    DOM-Form-Events lab - Final release

--- index.js
+++ index.js
@@ -1,50 +1,62 @@
 
-
-const taskInput = document.querySelector('textarea'),
-      tasks     = document.querySelector('#tasks');
-      error     = document.querySelector('p');
-      form      = document.getElementById('new-task');
-taskInput.focus();
+const tasksInput = document.querySelector('textarea'),
+      tasksList  = document.querySelector('#tasks');
+      error      = document.querySelector('p');
+      form       = document.querySelector('#new-task');
+tasksInput.focus();
 // Adding new task to the list
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
-    if(taskInput.value){
-        const task = document.createElement("li");
-        const taskData = document.createElement('span');
-        let deleteBtn = document.createElement("button");
-        deleteBtn.innerHTML = "Delete Task";
-        //task.appendChild(document.createTextNode(taskInput.value)).after(deleteBtn);
-        taskData.innerText = taskInput.value;
-        task.appendChild(taskData).after(deleteBtn);
-        tasks.append(task)
-        taskInput.value = '';
-        taskInput.focus();
+    if(tasksInput.value){
+        const tasks = tasksInput.value.split('\n')
+        tasks.forEach((task) => {
+            newTask(task)
+        })
+        
+        form.reset()
+        tasksInput.focus();
         error.textContent = '';
         error.classList.remove('error');
 
-        // Binding events to list itmes 
-        // => Strike task
-        task.addEventListener("click", () => {
-            task.setAttribute(`style`, `text-decoration: line-through`)
-        });
-        // => Delete task
-        if(tasks.innerHTML!==""){
+        // => Binding delete list item event
+        if(tasksList.innerHTML!==""){
             const deleteBtns = document.querySelectorAll('#tasks li button');
             for(let btn of deleteBtns){
                 btn.addEventListener('click', (e) => {
                     e.preventDefault();
                     btn.closest('li').remove();
-                    taskInput.focus();
+                    tasksInput.focus();
                 })
             }
         }
-        
     }else{
         error.classList.add('error');
         error.innerHTML = '<span>Error. Todo cannot be empty</span>';
-        taskInput.focus();
+        tasksInput.focus();
     }
-}) 
-console.log('>' +tasks.innerHTML)
+})
 
+function newTask(item){
+    const taskItem  = document.createElement("li"),
+          deleteBtn = document.createElement("button"),
+          iconItem  = document.createElement("i");
+    deleteBtn.innerHTML = '<i class="fa fa-solid fa-trash-can"></i>';
+    
+    iconItem.classList.add('fa', 'fa-solid', 'fa-check')
+    taskItem.textContent = item;
+    taskItem.appendChild(iconItem);
+    taskItem.appendChild(deleteBtn);
+    tasksList.append(taskItem)
+    // => Binding list item event
+    taskItem.addEventListener("click", () => {
+        if(!taskItem.hasAttribute('style')){
+            taskItem.setAttribute('style', 'text-decoration: line-through;color: #fe4a49;');
+            iconItem.classList.remove('fa-check');
+            iconItem.classList.add('fa-x')
+        }else{
+            taskItem.setAttribute('style', 'text-decoration: none solid rgb(0, 0, 0);');
+            iconItem.classList.remove('fa-x');
+            iconItem.classList.add('fa-check');
+        }
+    });
+}
