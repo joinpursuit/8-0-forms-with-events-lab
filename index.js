647a8c26da574836403819833e55ee183343dfca
Zane <zaneahmed4@gmail.com>
2022-04-02 15:03:06 -0400

    done without bonus

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log("Code your solution!")
+
+
+const form = document.querySelector("#my-form")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const ul = document.querySelector("ul")
+    const li = document.createElement("li")
+    li.textContent = event.target.list.value
+   if(li.textContent === ""){
+        const error = document.createElement("p")
+        error.textContent = 'Error. Todo cannot be empty'
+        ul.before(error)
+    }
+     ul.append(li)
+
+     li.addEventListener("click", (event) => {
+         event.target.style.textDecoration = "line-through"
+     })
+})
