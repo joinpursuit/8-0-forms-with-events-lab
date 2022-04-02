25e0694095fe7d2102737f46dae838f1d0965534
ziqian pan <ziqianpan1998@gmail.com>
2022-04-02 15:25:49 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("Code your solution!")
+//console.log("Code your solution!")
+
+const form = document.querySelector("#toDoList");
+const ul = document.querySelector("#list");
+const input = document.querySelector("input");
+const p = document.querySelector("p");
+
+
+form.addEventListener('submit', (event) => {
+event.preventDefault();
+
+    if (input.value === ""){
+        p.textContent = "Error. Todo cannot be empty";
+    }
+
+    const li = document.createElement("li");
+    //console.log(li);
+    li.textContent = event.target.task.value;
+    ul.append(li);
+    form.reset();
+});
+
+//const allLi = document.querySelectorAll('li');
+//console.log(allLi);
+
+    ul.addEventListener("click", (event) => {
+        event.target.setAttribute("style","text-decoration: line-through");
+    })
+
+
+
+
+
+
+
+
+
