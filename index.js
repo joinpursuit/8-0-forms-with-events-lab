a82c205136320543450df20e06b22d66f68129c1
Khaleed Uddin <uddin1786@gmail.com>
2022-04-02 17:41:51 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Code your solution!")
+function toDoList(toDo) {
+    const li = document.createElement("li");
+    li.textContent += toDo;
+    return li;
+}
+function generateToDo(toDo) {
+    const li = toDoList(toDo);
+    const ul = document.querySelector("ul");
+    ul.append(li);
+
+   
+    li.addEventListener('click', (event) =>{
+    li.setAttribute("style", "text-decoration: line-through solid rgb(0,0,0") 
+    })
+
+}
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+    // console.log(event.target)
+  event.preventDefault();
+generateToDo(event.target.toDo.value)
+form.reset();
+})
+
