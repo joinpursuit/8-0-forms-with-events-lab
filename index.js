4dc1e8d003ecf03e6b442ccb0d20f0a5e8fbdf3f
Jamal Ruiz <jamalruiz@jamals-air-2.mynetworksettings.com>
2022-04-03 16:15:40 -0400

    lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Code your solution!")
+const userform = document.querySelector("#user-form")
+userform.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    const userinput = event.target["Todo-field"].value;
+
+    const li= document.createElement("li");
+    li.textContent = userinput;
+    
+    const ul = document.querySelector("ul");
+    ul.append(li);
+li.addEventListener("click",(event) => {
+    li.style.textDecoration="line-through";
+})
+});
