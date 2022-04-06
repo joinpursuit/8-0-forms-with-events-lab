3e8d84e52dc834674cebb1cc9c3b6598e34b3ff9
Kalilah Clarke <kalilah.clarke@gmail.com>
2022-04-06 14:30:55 -0400

    Complete Bonus

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ h1.innerText = "My To-Dos";
 const ul = document.createElement("ul");
 // - A `form` for the user to add a new to-do, with a single text `input` and a `submit` button.
 const form = document.createElement("form");
-const textBox = document.createElement("input");
-textBox.setAttribute("type", "text");
-const button = document.createElement("button");
+const textBox = document.createElement("textarea");
+//textBox.setAttribute("type", "text");
+const button = document.createElement("input");
 button.setAttribute("type", "submit");
 button.textContent = "Submit";
 
@@ -25,14 +25,48 @@ body.append(ul);
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const li = document.createElement("li");
-  li.textContent += `${textBox.value}`;
-  ul.append(li);
+  console.log("In Event");
+  //const li = document.createElement("li");
+  // const br = document.createElement("br");
+  const toDoList = () => {
+    //console.log("Inside TODOLIST");
+    if (textBox.value.includes(`\n`)) {
+      textBox.value.split("\n").forEach((text) => {
+        const li = document.createElement("li");
+        li.textContent = text;
+        console.log("Before list setup", li);
+        liSetUp(li);
+        ul.append(li);
+      });
+    } else {
+      const li = document.createElement("li");
+      // console.log(textBox.value);
+      li.textContent += `${textBox.value}`;
+      console.log("Before list setup", li);
+      liSetUp(li);
+      ul.append(li);
+    }
+  };
 
-  li.addEventListener("click", (event) => {
-    console.log(event.target);
-    li.style.textDecoration = "line-through";
-  });
-
+  const liSetUp = (li) => {
+    li.addEventListener("click", (event) => {
+      //console.log(event.target);
+      if (li.style.textDecoration === "line-through") {
+        li.style.textDecoration = "none";
+      } else {
+        li.style.textDecoration = "line-through";
+      }
+    });
+    const button1 = document.createElement("button");
+    button1.setAttribute("type", "click");
+    button1.textContent = "Delete";
+    li.append(button1);
+    button1.addEventListener("click", (event) => {
+      //console.log(event.target);
+      //ul.removeChild(li, button1);
+      li.remove();
+    });
+  };
+  toDoList();
   form.reset();
 });
