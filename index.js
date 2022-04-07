e2be41a508ab3166b4c917ebc3b0d734b5bc6235
KamNotCam <Kedari.a.matthews@gmail.com>
2022-04-07 17:57:41 -0400

    I'm really proud of this

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Code your solution!")
+const ul = document.querySelector("ul");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event);
+  const li = document.createElement("li");
+  li.textContent = event.target.todo.value;
+
+  ul.append(li);
+  li.addEventListener("click", (e) => {
+    li.style.textDecoration = "line-through";
+  });
+});
