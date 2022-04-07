2ea84603f1a7e150dc47c04cf1dfd453513ab12a
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-04-07 00:28:44 -0400

    .

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log("Code your solution!")
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
