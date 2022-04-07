a791c709e91de285a5d6113720d7ab13aec8c791
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-04-07 00:29:11 -0400

    .

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector("form");
     form.addEventListener("submit", (event) => {
       event.preventDefault();
+
+    
+      const input = document.querySelector("input");
+
+      if (input.value.length === 0) {
+          const paragraph = document.querySelector("p");
+          paragraph.textContent = "Error. To-do list is empty!";
+      } else{
+          const newTask = document.createElement("li");
+          const addTaskButton = document.createElement("button")
+          newTask.textContent = input.value;
+          document.querySelector("ul").appendChild(newTask);
+      }
+      input.value = "";
+    });
