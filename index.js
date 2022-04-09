09c42f9c8ba98be114076aaecbf110783c6a5b13
jede <jedebrahma@pursuit.org>
2022-04-09 08:24:08 -0400

    forms with events

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log("Code your solution!")
+console.log("Code your solution!");
+console.log("Code your solution!");
+
+const form = document.querySelector("#inputForm");
+const toDoList = document.getElementById("todos");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let text = event.target.textInput.value;
+  console.log(text);
+  if (!text) {
+    document.querySelector("#errorMessage").textContent =
+      "Please enter a valid message";
+  } else {
+    document.querySelector("#errorMessage").textContent = "";
+    text = text.split("\n");
+    for (let word of text) {
+      if (word === "") continue;
+      const count = document.querySelectorAll(".todos").length;
+      let newListItem = document.createElement("li");
+      let deleteButton = document.createElement("button");
+      deleteButton.textContent = "delete";
+
+      newListItem.textContent = word;
+      newListItem.setAttribute("class", "todos");
+      newListItem.setAttribute("id", `todo${count}`);
+      deleteButton.setAttribute("id", `${count}`);
+      deleteButton.setAttribute("class", "erase");
+      newListItem.append(deleteButton);
+
+      toDoList.append(newListItem);
+    }
+
+    form.reset();
+  }
+});
+toDoList.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.className === "todos") {
+    if (event.target.style.textDecoration === "line-through") {
+      event.target.style.textDecoration = "none";
+    } else {
+      event.target.style.textDecoration = "line-through";
+    }
+  } else if (event.target.className === "erase") {
+    document.getElementById(`todo${event.target.id}`).remove();
+  }
+});
