8cd118dc27baf984b71121483809c01582b83efc
Trevor Triumph <jumanji16y@aol.com>
2022-04-18 07:03:33 -0400

    Completed Forms with events lab all tests passed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Code your solution!")
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (event.target.todo.value) {
+    document.querySelector("p").textContent = "";
+    const newTodo = document.createElement("li");
+    newTodo.textContent = event.target.todo.value;
+    newTodo.addEventListener("click", () => {
+      if (newTodo.style.textDecoration !== "line-through") {
+        newTodo.style.textDecoration = "line-through";
+      } else {
+        newTodo.style.textDecoration = "none";
+      }
+    });
+    const deleteButton = document.createElement("button");
+    deleteButton.addEventListener("click", (eventDel) => {
+      newTodo.parentNode.removeChild(newTodo);
+    });
+    deleteButton.style.marginLeft = "10px";
+    deleteButton.textContent = "Delete";
+    newTodo.append(deleteButton);
+    document.querySelector("ul").append(newTodo);
+  } else {
+    document.querySelector("p").textContent = "Error.Todo cannot be empty";
+  }
+  form.reset();
+});
