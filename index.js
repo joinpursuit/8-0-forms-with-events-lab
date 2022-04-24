c4c80352dca5cd1280db352ac531be272ce4f9ca
King Ankoue <kingankoue@Kings-Air.home>
2022-04-24 18:33:29 -0400

    formseventswithlabs

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log("Code your solution!")
+ document.addEventListener('DOMContentLoaded', () =>{
+     const form = document.querySelector('form');
+     const ul = document.querySelector('ul');
+     const p = document.querySelector('p');
+
+
+     form.addEventListener('submit' , e => {
+         e.preventDefault();
+
+         let input = document.getElementById('input').value;
+
+         if(!input) {
+             p.innerText = 'Error: Insert Text';
+         } else {
+             let li = document.createElement('li');
+             li.innerText = input;
+             ul.appendChild(li);
+         };
+     });
+
+     ul.addEventListener('click' , e => {
+         e.target.style.textDecoration = 'line-through'
+     });
+ });
