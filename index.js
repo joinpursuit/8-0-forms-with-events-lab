ea149ed04e0bc12419ec54f162cfc6dbb7588a23
sreeves07 <shamarreeves@pursuit.org>
2022-08-24 13:21:27 -0400

    form made

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Code your solution!")
+// console.log("Code your solution!")
+
+const form = document.querySelector('#new-list-form')
+
+form.addEventListener('submit',(event) => {
+  event.preventDefault()
+  const name = event.target.item.value
+  generateContact()
+})
+
+function contactTemplate (item, notes) {
+    const li = document.createElement("li");
+    li.textContent += item;
+
+      if (notes) {
+      li.append(document.createElement("br"), notes);
+    }
+
+    return li;
+  }
+
+function generateContact (item, notes) {
+    const li = contactTemplate(item, notes);
+  
+    const ul = document.querySelector("ul");
+    ul.append(li);
+  
+    UpdateContactCount()
+  }
