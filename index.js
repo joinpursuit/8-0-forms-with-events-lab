85eb4b4cb282198d4b80a787253ede423143d002
Jonathan-Gaal <jonathangaal@pursuit.org>
2022-08-24 15:52:30 -0400

    passed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Code your solution!")
+
+// const toDoBox = document.querySelector(`#toDoBox`); //text box
+const submitBtn = document.querySelector(`#submit`); //submit butn
+const toDoList = document.querySelector(`#toDoList`); //unordered list
+const form = document.querySelector(`form`);
+
+
+// li.textContent += toDo;
+
+// if (toDoBox)
+
+
+form.addEventListener(`submit`, (event) => {
+    event.preventDefault();
+    const listItem = document.createElement(`li`);
+    listItem.textContent += event.target.toDoBox.value;
+    toDoList.append(listItem);
+listItem.addEventListener(`click`, (event) => {
+    listItem.style.textDecoration = `line-through`
+})
+})
+
+
+// toDoBox.reset();
+  
