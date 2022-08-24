ef1142a917c85cec4e2b47c44f59d8f1055bd6a8
Fadila-Ali <fadilaali@pursuit.org>
2022-08-24 17:07:58 -0400

    Forms with events lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Code your solution!")
+// console.log("Code your solution!")
+const form = document.querySelector("form");
+console.log(form);
+
+const ul = document.querySelector("ul");
+console.log(ul);
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const li = document.createElement("li");
+  li.textContent = event.target.todo.value;
+  ul.append(li);
+
+  li.addEventListener("click", () => {
+    li.style.textDecoration = "line-through";
+  });
+
+  form.reset();
+});
