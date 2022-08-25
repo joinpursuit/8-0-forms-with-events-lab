31085f7ea7ae0ef84de87720aff130f52ff0cf81
Yiannar <yiannarichey@pursuit.org>
2022-08-25 09:33:07 -0400

    Finished lab with Melanie, Nekish, Dan, and Becky

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Code your solution!")
+// console.log("Code your solution!")
+
+const form = document.querySelector('form')
+console.log(form)
+
+
+const ul = document.querySelector('ul')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(event)
+    const li = document.createElement("li");
+    li.textContent = event.target.todo.value
+    li.classList.add("list")
+    ul.append(li);
+    form.reset()
+})
+
+ul.addEventListener('click', (event) => {
+    if (event.target.classList.contains("list")) {
+        event.target.style.textDecoration = "line-through"
+    }
+})
