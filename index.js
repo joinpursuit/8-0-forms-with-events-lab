f0aa9f39b64025e4ef30a29ba1a3d255f4ae7259
D-Stukes <dstukes888@gmail.com>
2022-08-25 09:58:46 -0400

    completed and passed test - only have bonus left

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("Code your solution!")
+
+
+
+// console.log("Code your solution!")
+const form = document.getElementById("todos")
+const ul = document.querySelector('ul')
+const button = document.querySelector('button')
+form.addEventListener("submit", (event) =>{
+    event.preventDefault()
+    if(!event.target.todo.value){
+        const errorMsg = document.createElement('p')
+        errorMsg.textContent = "This textbox cannot be left empty" 
+        button.after(errorMsg)
+    }
+
+    const li = document.createElement("li")
+    const deleteIt = document.createElement("button")
+   // li.textContent = input
+    console.log(event)
+    li.textContent = event.target.todo.value
+    ul.append(li)
+    li.append(deleteIt)
+    deleteIt.setAttribute("id", `${event.target.todo.value}`)
+    li.setAttribute("id", `${event.target.todo.value}`)
+    
+    //deleteIt.setAttribute
+    deleteIt.addEventListener('click', (event => {
+    const itemsBeGone = document.getElementById(`${deleteIt.id}`)
+    itemsBeGone.remove()
+    }))
+    li.addEventListener('click', (event) =>{li.style.textDecoration = 'line-through'
+
+          })
+    
+    form.reset()
+
+})
