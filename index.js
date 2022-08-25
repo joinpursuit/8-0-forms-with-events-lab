8747a4e4844b31dae3b3fbe0014002f1e00a04e5
Amina-Moufakkir <aminamoufakkir@pursuit.org>
2022-08-25 08:54:22 -0400

    refactoring

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ let text = document.querySelector("input[type='text']");
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-
-  if (!text.value) {
-    paragraph.textContent = 'Error! Please fill in your to-do task.';
-  }
-
+  
   const li = document.createElement('li');
   li.textContent = text.value;
   ul.append(li);
