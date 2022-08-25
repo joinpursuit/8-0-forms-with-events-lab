76072e1d23fe0164021df166bd955ccc1653523e
Bo Latt <bolattny@gmail.com>
2022-08-25 01:18:48 -0400

    add bonus features

--- index.js
+++ index.js
@@ -5,22 +5,48 @@ let lis = document.querySelectorAll("li");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const todo = e.target.todo;
-  if (todo.value == "") {
+  const input = e.target.todo;
+
+  if (input.value == "") {
     const p = document.createElement("p");
     p.innerText = "Need some input";
     form.after(p);
   } else {
-    const li = document.createElement("li");
-    li.addEventListener("click", (e) => {
-      toggleUnderline(e.target);
-    });
-    li.textContent = todo.value;
-    ul.append(li);
-    form.reset();
+    const inputTodos = input.value.trim().split("\n");
+    console.log(inputTodos);
+    for (let todo of inputTodos) {
+      createTodoAndAppend(todo);
+    }
+
+    input.value = "";
   }
 });
 
 function toggleUnderline(li) {
-  li.style.textDecoration = "line-through";
+  li.style.textDecoration =
+    li.style.textDecoration == "line-through" ? "none" : "line-through";
+}
+
+function deleteLi(li) {
+  li.parentElement.remove();
+}
+
+function createTodoAndAppend(todoText) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const deleteBtn = document.createElement("button");
+
+  li.addEventListener("click", (e) => {
+    toggleUnderline(e.target);
+  });
+
+  deleteBtn.addEventListener("click", (e) => {
+    deleteLi(e.target);
+  });
+  deleteBtn.textContent = "Delete";
+
+  span.textContent = todoText;
+  li.append(span);
+  li.append(deleteBtn);
+  ul.append(li);
 }
