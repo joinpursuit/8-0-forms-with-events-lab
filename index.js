9ccdde41133862544a26316c30d2866d21d453e4
Amina-Moufakkir <aminamoufakkir@pursuit.org>
2022-08-25 08:46:59 -0400

    form-with-events lab completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Code your solution!")
+//console.log("Code your solution!")
+
+let form = document.querySelector('form');
+let ul = document.querySelector('ul');
+let text = document.querySelector("input[type='text']");
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (!text.value) {
+    paragraph.textContent = 'Error! Please fill in your to-do task.';
+  }
+
+  const li = document.createElement('li');
+  li.textContent = text.value;
+  ul.append(li);
+
+  li.addEventListener('click', () => {
+    li.style.textDecoration = 'line-through';
+  });
+});
