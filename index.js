f5282a84d7808954218dee222b3389701b5e325b
Bo Latt <bolattny@gmail.com>
2022-08-24 21:21:23 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Code your solution!")
+const form = document.querySelector("form");
+const ul = document.querySelector("ul");
+let lis = document.querySelectorAll("li");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const todo = e.target.todo;
+  if (todo.value == "") {
+    const p = document.createElement("p");
+    p.innerText = "Need some input";
+    form.after(p);
+  } else {
+    const li = document.createElement("li");
+    li.addEventListener("click", (e) => {
+      toggleUnderline(e.target);
+    });
+    li.textContent = todo.value;
+    ul.append(li);
+    form.reset();
+  }
+});
+
+function toggleUnderline(li) {
+  li.style.textDecoration = "line-through";
+}
