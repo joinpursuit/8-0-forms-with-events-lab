bb004082c5b7e2c00ae1b141cd17b024449e4ab3
i-guerrero <isaacguerrero@pursuit.org>
2022-08-26 23:36:05 -0400

    Add eventListener for newLi strikethrough

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const newLi = document.createElement("li");
   const todo = event.target.todo.value;
   if (todo) {
+    const newLi = document.createElement("li");
     newLi.innerText = todo;
     const ul = document.querySelector("ul");
     ul.append(newLi);
+
+    // Add event listener on newLi
+    newLi.addEventListener("click", (newLiClickEvent) => {
+      newLiClickEvent.target.style.textDecoration = "line-through";
+    });
   } else {
     const errorMessage = document.createElement("p");
     errorMessage.innerText = "Error! Todo cannot be empty";
