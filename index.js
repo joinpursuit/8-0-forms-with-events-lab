f98ea5939708077cd2751983244d747ae72274a6
i-guerrero <isaacguerrero@pursuit.org>
2022-08-26 23:24:49 -0400

    Add eventListener for submit button

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Code your solution!")
+console.log("Code your solution!");
+
+// Create an event listener for submit button
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const newLi = document.createElement("li");
+  const todo = event.target.todo.value;
+  if (todo) {
+    newLi.innerText = todo;
+    const ul = document.querySelector("ul");
+    ul.append(newLi);
+  } else {
+    const errorMessage = document.createElement("p");
+    errorMessage.innerText = "Error! Todo cannot be empty";
+    event.target.after(errorMessage);
+  }
+});
