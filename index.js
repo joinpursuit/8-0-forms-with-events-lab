c3b308f6b2330aa215a3d456dd802d5ac7d217f3
Victor Rey <victorrey@pursuit.org>
2022-08-28 10:59:49 -0400

    finished

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 // console.log("Code your solution!")
 const form= document.querySelector("form");
+
 const ul= document.querySelector("ul");
-let lis= document.querySelectorAll("li");
+
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    
+    let lis= document.querySelectorAll("li");
 
     const li = document.createElement("li");
+
     li.textContent = event.target.todo.value;
+
     li.classList.add("list");
+    
     ul.append(li);
 
-    ul.addEventListener("click", event => {
-        if(event.target.classList.contains("list")) {
-            event.target.style.textDecoration = "line-through"
-        }
+    li.add.EventListener("clcik", () => {
+        li.style.textDecoration = "line-through";
     })
+
+    // ul.addEventListener("click", event => {
+    //     if(event.target.classList.contains("list")) {
+    //         event.target.style.textDecoration = "line-through"
+    //     }
+    // })
 });
