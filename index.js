7b12032259b24d639e7026548a3fc77abf248138
youssefmohamedfarouk <youssefmohamedfarouk@pursuit.org>
2022-08-28 16:28:18 -0400

    Add to-do list functionality in index.js and index.html and styling in styles.css

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log("Code your solution!")
+// let userInput;
+const toDoList = document.createElement('ul');
+const input = document.querySelector("form");
+input.after(toDoList);
+
+const form = document.querySelector("#to-do");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  function createListElem(inpt) {
+    const thingToDo = document.createElement('li');
+    const itemDelete = document.createElement('button');
+
+    itemDelete.addEventListener("click", (event) => {
+        thingToDo.remove();
+    });
+
+    thingToDo.textContent = inpt;
+
+    thingToDo.addEventListener("click", (event) => {
+        thingToDo.style.textDecoration = "line-through";
+    });
+
+    itemDelete.textContent = "Delete";
+    itemDelete.style.marginLeft = "25px";
+    thingToDo.append(itemDelete);
+
+    toDoList.append(thingToDo);
+    event.target.reset();
+
+    thingToDo.classList.add("list-elem");
+    itemDelete.classList.add("button-elem")
+  }
+
+
+  const userInput = document.querySelector("#user-input-id");
+  
+  if (!userInput.value) {
+    const p = document.createElement('p');
+    p.textContent = 'Error. Todo cannot be empty';
+    toDoList.before(p);
+  } else if (userInput.value.match(/\n/)) {
+    const allInputs = userInput.value.split(/\n/g);
+    for (const input of allInputs) {
+        createListElem(input);
+    }
+  } else {
+    createListElem(userInput.value);
+  }
+});
+
+// CSS Styling
+
+
