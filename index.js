2966cdcf2c4258b34824d89ddaa23fa2646b0a2b
Kylie-Lang1 <kylielang@pursuit.org>
2022-08-30 12:14:22 -0400

    Performs function, but no tests passed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Code your solution!")
+const form = document.querySelector(`form`)
+const ul = document.querySelector(`ul`)
+
+form.addEventListener(`submit`, (event) => {
+    event.preventDefault()
+
+    const li = document.createElement(`li`)
+    li.textContent = event.target.todo.value
+    ul.append(li)
+
+    li.addEventListener(`click`, () => {
+        if(li.style.textDecoration === `line-through`){
+            li.style.textDecoration = `none`}
+        else {
+            li.style.textDecoration = `line-through`
+        }
+    })
+
+    form.reset()
+})
