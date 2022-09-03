1e8a7da49c47b94416e764f75d20c6db83fadf83
SasukeDaTony <tonytaylor@pursuit.org>
2022-09-03 13:58:27 -0400

    initial-without-bonus

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Code your solution!")
+const form = document.querySelector("form");
+const list = document.querySelector("ul");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const newTask = document.createElement("li");
+    newTask.innerText = event.target.todo.value;
+    list.append(newTask);
+    newTask.addEventListener("click",() => {
+        newTask.style.textDecoration = "line-through";
+    })
+    if (newTask === "line-through") {
+        newTask.addEventListener("click",() => {
+            newTask.style.textDecoration = "none";
+        })
+    }
+} )
+
+
