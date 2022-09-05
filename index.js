091f2857c65e4c2e5d5d170bd5dc23db55ecc5c1
Brandon Bennett <107384180+codinghashira1120@users.noreply.github.com>
2022-09-04 21:25:25 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Code your solution!")
+
+const form = document.querySelector("form")
+
+const ul = document.querySelector("ul")
+
+form.addEventListener("submit", (event) => {
+event.preventDefault();
+
+// if (event.target.text.value === "") {
+//     const p = document.createElement("p")
+//     p.textContent = "Error. To-do cannot be empty"
+//     p.before(ul)
+// }
+
+const li = document.createElement("li")
+    li.textContent = event.target.text.value
+ul.append(li)
+
+li.addEventListener("click", () => {
+    li.style.textDecoration = "line-through"
+}
+
+)})
+
+
+
+
