3b5147b7063eb49550d87528857eb91c83e6cc5d
bigdaddyjean <jeannappoleon@pursuit.org>
2022-09-06 20:36:10 -0400

    finished

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Code your solution!")
+const button = document.getElementById('button');
+const inputo = document.getElementById('to-do');
+const task = document.getElementById('toDoContainer')
+
+
+
+button.addEventListener('click', function () {
+    // event.preventDefault();
+
+    let li = document.createElement('li');
+    li.innerText = inputo.value;
+
+    task.appendChild(li);
+
+    inputo.value = "";
+
+    li.addEventListener('click', function() {
+      li.style.textDecoration = "line-through";
+     })
+
+     li.addEventListener('dblclick', function() {
+        task.removeChild(li);
+     })   
+}
+)
+
+
