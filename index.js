d2f838b092732df690f6689671f9ad7c5a4d041c
jamesscott@pursuit.org <Jimmy@2019mbp.local>
2022-09-08 23:23:02 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Code your solution!")
+// console.log("Code your solution!")
+
+let form = document.querySelector('form');
+let ul = document.querySelector('ul');
+let text = document.querySelector("input[type='text']");
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const li = document.createElement('li');
+  li.textContent = text.value;
+  ul.append(li);
+
+  li.addEventListener('click', () => {
+    li.style.textDecoration = 'line-through';
+  });
+});
