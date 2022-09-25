65a6944b1fc57929d47ee0551fd12fcf1f0f374a
ZayLpursuit <Zaylors@pursuit.org>
2022-09-25 12:21:16 -0400

    all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log("Code your solution!")
+
+const form=document.querySelector("form")
+const body=document.querySelector("body")
+ const ul = document.querySelector("ul");
+ const err=document.querySelector("#error")
+
+ form.addEventListener("submit",(event)=>{
+    event.preventDefault()
+
+    
+    if (event.target.chore.value){
+    const li = document.createElement('li')
+ li.textContent = event.target.chore.value
+ ul.append(li)
+ li.addEventListener('click', (event) => {
+ li.style.textDecoration = "line-through"
+ })
+}
+ if (!event.target.chore.value){
+    err.textContent="Error. To do cannot be empty"
+ }
+
+
+ })
