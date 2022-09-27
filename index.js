d0dbadf5909ef2c33dab5bbe11289aac35d814de
pursuit-monique <moniquecorrea@pursuit.orgname>
2022-09-26 22:31:36 -0400

    DON'T USE SINGLE QUOTES.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Code your solution!")
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  let li = document.createElement("li");
+  li.textContent = `${document.querySelector("input[type=text]").value}`;
+  li.addEventListener("click", (event) => {
+    event.preventDefault();
+    li.style.textDecoration = "line-through";
+    console.log("this worked.");
+  });
+  document.querySelector("ul").append(li);
+});
