69332f81c2d4ae536b2c09f6acb04ae3ca219d12
JenniferEinstein <jennifereinstein@gmail.com>
2022-12-14 21:59:39 -0500

    just starting out, but at least I got a button

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("Code your solution!")
+document.forms.submit
+
+function addTodo(button){
+    let addThis = document.todoForm.display.value
+    addThis = "put an LI tag here."
+}
+//this function, once one item is added, a list must be created
+//add unordered list
+
+function createList{
+    add <UL>
+
+}
