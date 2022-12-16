ac0bf808d7fb1f3c5fd006c4233c1e1a783e3043
BertCasale <bthebest55@gmail.com>
2022-12-15 20:54:57 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 console.log("Code your solution!")
+
+//get the form
+const form = document.querySelector("form");
+//get the list
+const list = document.querySelector("ul");
+//get the error message paragraph
+const empty = document.getElementById("empty")
+
+form.addEventListener("submit", (event) => {
+    //tell it not to do what it normally would
+    event.preventDefault();
+    //add the item if the box isnt empty
+    event.target.todo.value ? addListItem(event) : showErrorMessage();
+})
+
+function addListItem(event) {
+     //create the new list item
+    const newItem = document.createElement("li");
+    //give it the value of the input box
+    newItem.textContent = event.target.todo.value;
+    //put the new item at the end of the list
+    list.append(newItem);  
+    //add click event to the newItem
+    newItem.addEventListener("click", () => {
+        newItem.style.textDecoration = "line-through";
+    })
+    empty.style.display = "none";
+}
+
+function showErrorMessage() {
+    empty.style.display = "inline";
+}
