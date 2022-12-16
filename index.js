973949fd7f9c35818ee447bf870fafd778171562
AlexTan21 <87435891+AlexTan21@users.noreply.github.com>
2022-12-16 13:00:42 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Code your solution!")
+const ul = document.createElement("ul");
+
+const form = document.querySelector("form");
+form.prepend(ul)
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const value = todo.value;
+    if (!value) {
+        const p = document.createElement("p");
+        p.textContent = "Error";
+        ul.prepend(p)
+    } else {
+        const li = document.createElement("li");
+        li.textContent = value;
+        ul.append(li);
+        li.addEventListener("click", (event) => {
+            if (event.target.style.textDecoration === "line-through") {
+                event.target.style.textDecoration = "blank"
+            } else {
+                event.target.style.textDecoration = "line-through"
+            }
+        })
+    }
+})
