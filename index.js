633bfa253238e722ede9cf34e2241006766a39f9
BertCasale <bthebest55@gmail.com>
2022-12-15 21:37:10 -0500

    first 3 bonus tasks

--- index.js
+++ index.js
@@ -18,16 +18,41 @@ function addListItem(event) {
      //create the new list item
     const newItem = document.createElement("li");
     //give it the value of the input box
-    newItem.textContent = event.target.todo.value;
+    newItem.textContent = event.target.todo.value + " ";
+
+    //BONUS
+    //clear the input box
+    event.target.todo.value = "";
+
     //put the new item at the end of the list
     list.append(newItem);  
+
+    //BONUS
+    //make a button
+    const delBut = document.createElement("button");
+    //set name on button
+    delBut.textContent = "Delete";
+    //put button at the end of the li element
+    newItem.append(delBut);
+    //tell button what to do when clicked
+    delBut.addEventListener("click", (event) => {
+        event.preventDefault();
+        //remove the whole list item
+        newItem.remove();
+    })
+
     //add click event to the newItem
     newItem.addEventListener("click", () => {
-        newItem.style.textDecoration = "line-through";
+        //HALF BONUS HALF REQUIRED
+        //toggles crossout when clicked
+        newItem.style.textDecoration === ("line-through") ? newItem.style.textDecoration = "none" : newItem.style.textDecoration = "line-through";
     })
+    //make the error message go away
     empty.style.display = "none";
 }
 
 function showErrorMessage() {
+    //make the error message show up
     empty.style.display = "inline";
 }
+
