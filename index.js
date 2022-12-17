1b530ae434aea8de55d050243bd4a7b590d8695d
Keeanu Rodriguez <keeanu7@gmail.com>
2022-12-17 13:39:01 -0500

    task extra extra done

--- index.js
+++ index.js
@@ -10,8 +10,34 @@ form.addEventListener("submit", (event) => {
     p.innerText = ""; 
 
     const li = document.createElement("li"); 
-    li.textContent = event.target.text.value;  
-    ul.append(li);
+    if(text.value.includes("\n")){
+        let multiLine = text.value.split("\n")
+        const li2 = document.createElement("li"); 
+        li.textContent = multiLine[1]
+        li2.textContent = multiLine[0]
+        ul.append(li2)
+        ul.append(li)
+
+        const deleteBtn2 = document.createElement("button"); 
+        li2.append(deleteBtn2); 
+        deleteBtn2.innerText = "delete"; 
+
+        deleteBtn2.addEventListener("click", () => {
+            li2.remove();
+        })
+
+        li2.addEventListener("click", () => {
+            if(li2.style.textDecoration === "line-through"){
+                li2.style.textDecoration = "none"
+            } else {
+                li2.style.textDecoration = "line-through"
+            }
+        })
+
+    } else {
+        li.textContent = event.target.text.value;
+        ul.append(li);
+    }
 
 
     const deleteBtn = document.createElement("button"); 
@@ -22,7 +48,7 @@ form.addEventListener("submit", (event) => {
         p.innerText = "Error To Do Can Not Be Empty!";
         li.remove();
     }
-
+    
     text.value = "";
 
     deleteBtn.addEventListener("click", () => {
