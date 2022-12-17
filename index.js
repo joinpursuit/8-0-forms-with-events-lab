1ee2b65be7d728f4fdddd300aab377961db3d2d6
paoAracena <Paolaaracena1233@gmail.com>
2022-12-17 16:21:32 -0500

    done :)

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Code your solution!")
+// const formAdded = document.querySelector('form')
+// const list = document.querySelector('ul')
+// function listItem() {
+//     formAdded.addEventListener('submit',(event) => {
+//     event.preventDefault();
+//     list.append(document.createElement('li'));
+
+//     const accessListItems= document.querySelector('li');
+//     accessListItems.textContent = document.form.input.value;
+//     })
+// }
+
+const formElement = document.getElementById('pao')
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let listElement = document.createElement('li');
+    const textInputValue = event.target.testInput.value;
+    //console.log(event.target.testInput.value)
+    //shows value on console 
+    listElement.textContent = textInputValue;
+    
+    listElement.addEventListener('click', handleClick =>{
+     handleClick.target.style.textDecoration = "line-through";
+    })
+
+    const unorderedListElement = document.querySelector('ul');
+    unorderedListElement.append(listElement);
+    //creation/ assigenment/functionally and then append
+    event.target.testInput.value ='';
+})
