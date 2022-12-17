3b710566c7897ae5f7883823afc9d2fb777eab57
christinacephus@pursuit.org <christinacephus@pursuit.org>
2022-12-17 13:30:08 -0500

    pseudo-coding my way to a solution

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Code your solution!")
+const kiwi = document.querySelector('form')
+const list = document.querySelector('ul')
+
+function listItems() {
+    kiwi.addEventListener('submit', (event)=> {
+    event.preventDefault();
+    list.append(document.createElement('li'))
+    const accessListItem = document.querySelector('li');
+    // accessListItem.append(document.form.kiwi.value)
+    accessListItem.textContent = document.form.kiwi.value;
+})
+}
+
+// do we need to set class attributes? to create list
+//
+//function newLine() {
+    //i want the text value to display as a <li> after the form use append
+    
+//}
+
