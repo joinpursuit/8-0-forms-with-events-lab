c057a591b75461ce66629aef73bea807915cde19
Jyoti Singh <jyo.singh88@gmail.com>
2022-12-16 20:09:26 -0500

    firstCommitNoBonus

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("Code your solution!")
+// console.log("Code your solution!")
+
+// const form = document.querySelector('form')
+// const toDo = document.querySelector('ul')
+// const button = document.querySelector('button')
+
+
+// form.addEventListener('submit', (event) => {
+// 	event.preventDefault();
+
+// 	const li = document.createElement('li')
+// 	// const input = event.target.toDoList.value
+// 	li.textContent = input.value;
+// 	toDo.append(li)
+
+// const newList = document.querySelectorAll('ul li');
+
+
+//       li.forEach((li) => {
+//          newList.style.textDecoration = 'line-through solid';
+//   toDo.addEventListener('click', () => {});
+// });
+// });
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const newList = document.querySelector('ul');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const myText = input.value;
+  const toDo = document.createElement('li');
+  toDo.textContent = myText;
+  newList.append(toDo);
+
+  newList.addEventListener('click', (event) => {
+    toDo.style.textDecoration = 'line-through solid';
+  });
+  form.reset();
+});
