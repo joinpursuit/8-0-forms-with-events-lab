724075d29ada97f01015e9126b015f620cab9638
joshuanelsondev <joshuanelsondev@gmail.com>
2022-12-18 16:35:36 -0500

    Update bonuses

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('form');
 const ul = document.querySelector('.todoList');
 const errorMessageField = document.querySelector('p.error');
+
 // Create a variable that will contain all of the 'li' elements 
 let todoListElements = [];
 
@@ -9,7 +10,7 @@ let todoListElements = [];
 form.addEventListener('submit', updateTodoList);
 
 // Create an event listener to reset the errorMessageField when the user clicks inside of the input field
-form.todoText.addEventListener('focus', removeErrorText);
+form.todoText.addEventListener('input', removeErrorText);
 
 // Write a function that will create an 'li' element for each user todo submission
 function updateTodoList(event) {
@@ -17,23 +18,31 @@ function updateTodoList(event) {
     
     // Create 'li' element 
     const li = document.createElement('li');
+    // Create a delete button that will go next to each li element
+    const todoDeleteButton = document.createElement('button');
+    const hr = document.createElement('hr');
+
 
     // Update the text for the 'li' element to be the same as the user todo submission text
     li.innerText = event.target.todoText.value;
 
     // Check to see if the input is empty and call printError() if it is empty, otherwise append the 'li' element to the 'ul' element
-    if (!li.innerText) {
+    if (form.todoText.value.trim() === "") {
         printError();
     } else {
         ul.append(li);
         todoListElements = document.querySelectorAll('li');
+        // Loop through each 'li' element and add an event listener for each element
+        todoListElements.forEach(todoLi => {
+            todoLi.addEventListener('click', strikeThrough);
+            todoDeleteButton.classList.add('deleteButton');
+            todoDeleteButton.textContent = 'DEL';
+            todoLi.append(todoDeleteButton);  
+        });
+        li.after(hr);
+        
     }
-    
-    // Loop through each 'li' element and add an event listener for each element
-    todoListElements.forEach(todoLi => {
-        todoLi.addEventListener('click', strikeThrough);
 
-    });
     
     // Reset the input field  and/or the errorMessageField after the user submits
     form.reset();
@@ -61,3 +70,6 @@ function strikeThrough(event) {
     event.target.style.textDecoration = 'none';
    }
 }
+
+
+
