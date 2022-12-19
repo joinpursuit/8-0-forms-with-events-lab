6dc0cdff5feb27541b529673e91b9df2678c040e
Ryan Lundy <rlundy1792@gmail.com>
2022-12-19 02:00:42 -0500

    passing all tests

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Code your solution!")
+const formElement = document.querySelector("form");
+formElement.addEventListener("submit", event => {
+    event.preventDefault();
+    
+    const toDoFormValue = event.target.toDoForm.value;
+
+    if (toDoFormValue === "") {
+        document.querySelector("p").textContent = "Error, please enter text."
+    } else {
+        const listEl = document.createElement("li");
+
+        document.querySelector("p").textContent = "";
+
+        listEl.textContent = event.target.toDoForm.value;
+
+        listEl.addEventListener("click", handleClick => {
+            handleClick.target.style.textDecoration = "line-through"
+        });
+
+        const unorderedList = document.querySelector("ul");
+
+        unorderedList.append(listEl);
+    }
+});
