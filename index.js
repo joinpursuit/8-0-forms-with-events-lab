a433eed0501bc3b783bf0ee6ee8da01917ebf03b
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-12-19 09:59:46 -0500

    Added main features

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const form
+const form = document.querySelector("form")
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    const listElements = document.createElement("li")
+    if(event.target.text.value === ""){
+        document.querySelector('p').textContent = "Error"
+    } else{
+        document.querySelector('p').textContent = ''
+        listElements.textContent = event.target.text.value
+
+        listElements.addEventListener('click', handleClick => {
+            handleClick.target.style.textDecoration = 'line-through'
+        })
+
+    let ulElement = document.querySelector('ul')
+    ulElement.append(listElements)
+    event.target.text.value = ""
+    }
+})
