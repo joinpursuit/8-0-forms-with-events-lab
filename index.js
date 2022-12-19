b841222442929033d00b1564eba364603ac62ddf
vg0at <cameronmedina0130@gmail.com>
2022-12-18 20:34:54 -0500

    initial commit, passes basic tests

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Code your solution!")
+
+const form = document.querySelector("form");
+const ul = document.querySelector("ul");
+
+
+//what event do we want to listen for?
+const button = document.querySelector("button")
+form.addEventListener("submit", (event) => {
+    //text input we will grab
+    const txt = document.querySelector("input[type='text']");
+
+    // console.log(txt.value)
+    event.preventDefault();
+    const li = document.createElement("li")
+    li.classList.add('added')
+    li.textContent = txt.value;
+
+    form.reset();
+
+    ul.append(li);
+})
+ul.addEventListener("click", (event) => {
+    if (event.target.classList.contains("added")) {
+        event.target.style.textDecoration = "line-through"
+    }
+})
+
