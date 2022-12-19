6b7e6769c4c61e159459942ce02d65140341fcbf
Diandre Miller <millerdiandre@gmail.com>
2022-12-19 01:35:04 -0500

    Completed Forms With Events Lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 console.log("Code your solution!")
+
+let formElement = document.querySelector('form');
+
+formElement.addEventListener('submit', event => {
+    event.preventDefault();
+
+    let liElement = document.createElement('li');
+
+    if (event.target.textInput.value === '') {
+        document.querySelector('p').textContent = "error"
+    } else {
+
+        document.querySelector('p').textContent = '';
+
+        liElement.textContent = event.target.textInput.value;
+        
+        liElement.addEventListener('click', handleClick => {
+            handleClick.target.style.textDecoration = 'line-through'
+        })
+
+    let ulElement = document.querySelector('ul');
+
+        ulElement.append(liElement);
+        
+        event.target.textInput.value = ''
+
+    
+
+    }
+
+    
+})
