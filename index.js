8b7b86dea9f09780bbb584619fa92551d39bbe3d
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-12-19 11:09:04 -0500

    Added main features

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ form.addEventListener("submit", event => {
     event.preventDefault();
     const listElements = document.createElement("li")
     if(event.target.text.value === ""){
-        document.querySelector('p').textContent = "Error"
+        document.querySelector('p').textContent = "Error - Empty String"
     } else{
         document.querySelector('p').textContent = ''
         listElements.textContent = event.target.text.value
@@ -12,8 +12,18 @@ form.addEventListener("submit", event => {
             handleClick.target.style.textDecoration = 'line-through'
         })
 
-    let ulElement = document.querySelector('ul')
-    ulElement.append(listElements)
-    event.target.text.value = ""
+        let ulElement = document.querySelector('ul')
+        ulElement.append(listElements)
+        event.target.text.value = ""
+        form.reset()
+        // $('#form').focus()
     }
 })
+
+// const list =  document.querySelector("li")
+// list.addEventListener("dblclick", event => {
+//     event.preventDefault()
+//     if(list.target.text.value !== ""){
+//         target.style.textDecoration = "none";
+//     }
+// })
