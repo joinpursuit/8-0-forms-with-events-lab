94782d13cd0b7cdc2f2e61d67d9f9629c0a733ba
Jyoti Singh <jyo.singh88@gmail.com>
2022-12-20 04:17:42 -0500

    finalcommit

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-// console.log("Code your solution!")
-
-// const form = document.querySelector('form')
-// const toDo = document.querySelector('ul')
-// const button = document.querySelector('button')
-
-
-// form.addEventListener('submit', (event) => {
-// 	event.preventDefault();
-
-// 	const li = document.createElement('li')
-// 	// const input = event.target.toDoList.value
-// 	li.textContent = input.value;
-// 	toDo.append(li)
-
-// const newList = document.querySelectorAll('ul li');
-
-
-//       li.forEach((li) => {
-//          newList.style.textDecoration = 'line-through solid';
-//   toDo.addEventListener('click', () => {});
-// });
-// });
 
 const form = document.querySelector('form');
 const input = document.querySelector('input');
@@ -30,13 +7,14 @@ const newList = document.querySelector('ul');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const myText = input.value;
+  const myText = `${input.value}`;
   const toDo = document.createElement('li');
+
   toDo.textContent = myText;
   newList.append(toDo);
-
-  newList.addEventListener('click', (event) => {
-    toDo.style.textDecoration = 'line-through solid';
+  toDo.addEventListener('click', (event) => {
+    event.preventDefault();
+    toDo.style.textDecoration = 'line-through';
   });
   form.reset();
 });
