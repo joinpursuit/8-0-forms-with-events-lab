36bf3ecf6a51959d5ee41e2cc3a0ca2d6ed84b59
wal3sca <walescaambroise@gmail.com>
2022-12-21 12:53:31 -0400

    init commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Code your solution!")
+// console.log("Code your solution!")
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    const p = document.querySelector("p");
+    const ul = document.querySelector("ul");
+    const li = document.querySelector("li");
+    const entry = event.target.userInput.value;
+    if(entry.length === 0){
+        p.textContent = "error";
+    } else{
+        entry.textContent = "";
+        li.textContent = entry;
+        ul.append(li);
+    }
+    event.target.reset();
+})
