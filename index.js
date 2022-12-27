836b01c4e5ed26d4483c0567ee19c4fd02d17a9d
SamuelBAlba <brayanbalba24@gmail.com>
2022-12-26 20:22:55 -0500

    forms with events

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Code your solution!")
+//console.log("Code your solution!")
+
+// create variales for what needs to be manipulated
+const form = document.querySelector('form');
+const ul = document.querySelector('ul');
+
+// creates an event call back within form when the submit is triggered.
+form.addEventListener('submit', (event) => {
+    // prevents page from default action
+    event.preventDefault();
+    // the callback is to create an li
+    const li = document.createElement('li');
+    // the text of the li will be the value of what user inputted 
+    li.textContent = event.target.todo.value;
+    // append the new li to ul
+    ul.append(li);
+
+    // add an event to the li using click
+    li.addEventListener('click', () => {
+        // when the li is clicked a line through should appear
+        li.style.textDecoration = 'line-through';
+      });
+
+    })
