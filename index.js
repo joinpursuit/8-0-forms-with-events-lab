36f1b99649294d3641e275d6f0aa03885b0b8dd1
Wilsantos1975 <Faustosantos@pursuit.org>
2022-12-28 22:21:23 -0500

    work on getting the input value

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Code your solution!")
+const h1 = document.querySelector("h1");
+h1.innerText = "My To-Dos";
+let data = "";
+
+const addTodo = (event) => {
+  //event.preventDefault();
+  const input = document.getElementById(".todo");
+   console.log(input)
+};
+const ul = document.createElement("ul");
