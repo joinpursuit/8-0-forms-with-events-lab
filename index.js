aa74b07c085f978f9ab77942d65a982c776444c5
riyaalexander <riyaalexander@Riyas-MacBook-Air.local>
2022-12-30 13:18:29 -0500

    Forms with Event

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Code your solution!")
+//console.log("Code your solution!")
+
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const form = document.querySelector('form')
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const li = document.createElement("li")
+    li.textContent = input.value
+    document.querySelector("ul").append(li);
+
+    const list= document.querySelectorAll("ul li")
+
+    list.forEach((li) => {
+        li.addEventListener("click", () => {
+            li.setAttribute("style", "text-decoration: line-through");
+        })
+    })
+})
