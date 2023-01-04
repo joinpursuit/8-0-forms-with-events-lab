a4e3e0345bbaa49155d37c808fe2891cfbed77f2
MichelBDLC <michelbdlc@gmail.com>
2023-01-04 02:27:20 -0800

    added input form, preventing default. process creates list el & appends.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const input = document.querySelector("input[type='text']");
-input.addEventListener
+
+input.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const theLi = document.createElement("li")
+    theLi.textContent = input.value
+    document.querySelector("ul").append(myList);
+
+    const toDo = document.querySelectorAll("ul li")
+
+    for (let li of toDo) {
+        theLi.addEventListener("click", () => {
+            theLi.style.textDecoration = "line-through solid";
+        })
+    }
+
+    input.reset();
+})
