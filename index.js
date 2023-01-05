0170c2a4eb4646e836acc852b5259ba26af2303c
MichelBDLC <michelbdlc@gmail.com>
2023-01-04 23:04:09 -0800

    got the list part down finally yay

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-const input = document.querySelector("input[type='text']");
-const form = document.querySelector("form")
-
-input.addEventListener("submit", (event) => {
+document.querySelector("form").addEventListener("submit", (event) => {
     event.preventDefault();
 
-    //document.querySelector(input.value);
-
-    const li = document.createElement("li")
-    li.textContent = input.value
-    document.querySelector("ul").append(li);
-
-    li.addEventListener("click", () => {
-        li.style.textDecoration = "line-through";
-    });
-
-    //const theDo = document.querySelectorAll("ul li")
-
-    // theDo.forEach((li) => {
-    //     theDo.addEventListener("click", () => {
-    //         theDo.style.textDecoration = "line-through solid";
-    //     })
-    // })
-
-    // for (let li of theDo) {
-    //     theDo.addEventListener("click", () => {
-    //         theDo.style.textDecoration = "line-through solid";
-    //     })
-    // }
-     form.reset();
-
+    theUl = document.querySelector("ul")
+    liEl = document.createElement("li")
+    theUl.append(liEl)
+    liEl.textContent = event.target.form.value
+    
 })
+
+
 
