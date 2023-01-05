f7b4953e8b6c951fc34c3d5717ee606f425123b1
MichelBDLC <michelbdlc@gmail.com>
2023-01-04 19:17:22 -0800

    will have to revisit, tried different strategies, not passing tests

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const input = document.querySelector("input[type='text']");
+const form = document.querySelector("form")
 
 input.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const theLi = document.createElement("li")
-    theLi.textContent = input.value
-    document.querySelector("ul").append(myList);
+    //document.querySelector(input.value);
 
-    const toDo = document.querySelectorAll("ul li")
+    const li = document.createElement("li")
+    li.textContent = input.value
+    document.querySelector("ul").append(li);
 
-    for (let li of toDo) {
-        theLi.addEventListener("click", () => {
-            theLi.style.textDecoration = "line-through solid";
-        })
-    }
+    li.addEventListener("click", () => {
+        li.style.textDecoration = "line-through";
+    });
 
-    input.reset();
+    //const theDo = document.querySelectorAll("ul li")
+
+    // theDo.forEach((li) => {
+    //     theDo.addEventListener("click", () => {
+    //         theDo.style.textDecoration = "line-through solid";
+    //     })
+    // })
+
+    // for (let li of theDo) {
+    //     theDo.addEventListener("click", () => {
+    //         theDo.style.textDecoration = "line-through solid";
+    //     })
+    // }
+     form.reset();
+
 })
+
