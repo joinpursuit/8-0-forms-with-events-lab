d5ef597b0017b75d5aff8f3e64086760dae32190
mrkwab3 <mrkwab3@gmail.com>
2023-01-18 11:09:11 -0500

    Completed form with events lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Code your solution!")
+// user clicks 'add item' after entering text
+// obtain the user inputted text information
+// create a new list item
+// attach the user inputted text information to the new list item
+// attach the list item to the unordered list
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const text = document.querySelector('input[type="text"]')
+    const listItem = document.createElement('li')
+    const list = document.querySelector('.todo')
+
+    if (text.value.length === 0) { // If no text is inputted a paragraph error message will show up.
+        const p = document.createElement('p')
+        const body = document.querySelector('body')
+
+        p.textContent = 'Please input text into the text box before submitting!'
+        body.append(p)
+    } else {
+    listItem.textContent = text.value 
+    list.append(listItem)
+    }
+
+    const listItems = document.querySelectorAll('li')
+listItems.forEach((item) => {
+    item.addEventListener('click', (event) => {
+        item.setAttribute('style', 'text-decoration: line-through')
+    })
+})
+})
+
