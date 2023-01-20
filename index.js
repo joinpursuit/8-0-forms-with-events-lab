4561b9a6042675bfc318e90646d7a762e135a714
ryanfodera <ryanfodera@pursuit.org>
2023-01-20 11:21:19 -0500

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("Code your solution!")
+
+const form = document.querySelector('#todo');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const li = document.createElement('li');
+    li.textContent = event.target.todo.value;
+    const ul = document.querySelector('ul');
+    ul.append(li);
+
+    li.addEventListener('click', (event) => {
+        li.style.textDecoration = 'line-through'
+    });
+});
